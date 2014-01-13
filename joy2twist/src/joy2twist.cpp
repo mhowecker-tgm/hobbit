feb0d4b0e6ae5e0d170ff0e35608deebd72acc41
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-01-13 15:41:09 +0000

    Add joy2twist package
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@6186 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

---
+++ joy2twist/src/joy2twist.cpp
@@ -0,0 +1,72 @@
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Joy.h>
+#include <string.h>
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+class PS3Joystick2Twist
+{
+  public: 
+    PS3Joystick2Twist();
+    void SetFastSpeed(void);
+
+  private:
+    void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+
+    ros::NodeHandle nh_;
+    ros::NodeHandle priv_nh_;
+
+    int linear_axis_, angular_axis_;
+    double linear_, angular_;
+    ros::Publisher vel_pub_;
+    ros::Subscriber joy_sub_;
+};
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+PS3Joystick2Twist::PS3Joystick2Twist() : priv_nh_("~")
+{
+  linear_ = 0.2; //0.15;
+  angular_ = 0.41888; //0.31416;
+  linear_axis_ = 1;
+  angular_axis_ = 0;
+
+  priv_nh_.param("linear_axis", linear_axis_, linear_axis_);
+  priv_nh_.param("angular_axis", angular_axis_, angular_axis_);
+  priv_nh_.param("linear_speed_max", linear_, linear_);
+  priv_nh_.param("angular_speed_max", angular_, angular_);
+
+  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &PS3Joystick2Twist::joyCallback, this);
+}
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+void PS3Joystick2Twist::SetFastSpeed(void)
+{
+  linear_ = 0.4;
+  angular_ = 0.6283;
+}
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+void PS3Joystick2Twist::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
+{
+  geometry_msgs::Twist twist;
+  twist.linear.x = linear_ * joy->axes[linear_axis_];
+  twist.angular.z = angular_ * joy->axes[angular_axis_];
+  vel_pub_.publish(twist);
+}
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+int main(int argc, char * argv[])
+{
+  ros::init(argc, argv, "ps3joystick2twist");
+  PS3Joystick2Twist teleop_harlie;
+
+  if (argc == 2)
+  {
+    if (strcmp(argv[1], "--fast") == 0)
+    {
+      printf("Using fast mode!\n");
+      teleop_harlie.SetFastSpeed();
+    }
+  }
+
+  ros::spin();
+}
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
---
+++ joy2twist/src/joybutton.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python
+
+## walter wohlkinger
+## first version: 27.04.2011
+
+PKG = 'joy2twist'
+import roslib; roslib.load_manifest(PKG)
+import rospy
+import sys
+import numpy
+import serial
+from std_msgs.msg import String, Header
+from sensor_msgs.msg import Joy
+from hobbit_msgs.msg import Command, Status, Event, Parameter
+
+pubA = rospy.Publisher("/ActionSequence", Command)
+pubC = rospy.Publisher("/Command", Command) 
+pubE = rospy.Publisher("/Event", Event)    
+oneclick = True
+
+def joyCallback(msg):
+    global oneclick
+    if numpy.sum(numpy.asarray(msg.buttons)) < 1.0:
+        #print 'True'
+        oneclick = True
+        
+    if oneclick == False:
+        #print 'returning'
+        return
+    
+
+    if msg.buttons[12] == 1:    #triangle right
+        e = Event()
+        e.header = Header()
+        e.event = "G_YES"
+        e.sessionID = 'gesture_recognized'
+        e.confidence = 1
+        pubE.publish(e)
+        print e.event
+        oneclick = False
+
+
+    if msg.buttons[11] == 1:    #1-button right
+        e = Event()
+        e.header = Header()
+        e.event = "G_NO"
+        e.sessionID = 'gesture_recognized'
+        e.confidence = 1
+        pubE.publish(e)
+        print e.event
+        oneclick = False
+
+
+    if msg.buttons[10] == 1:    #1-button left
+        a = Command()   #action sequenz
+        a.header = Header()
+        a.command = "E_CALLHOBBIT"
+        a.sessionID = 'abc'
+        pubA.publish(a)
+        print a.command
+        oneclick = False
+
+    if msg.buttons[9] == 1:     #2-botton right
+        e = Event()
+        e.header = Header()
+        e.event = "G_FALL"
+        e.sessionID = 'gesture_recognized'
+        e.confidence = 1
+        pubE.publish(e)
+        print e.event
+        oneclick = False
+        
+    if msg.buttons[15] == 1:
+        e = Command()
+        e.header = Header()
+        e.command = "F_SETTINGS"
+        e.sessionID = '0'
+        pubC.publish(e)
+        print e.command
+        oneclick = False
+
+    if msg.buttons[13] == 1:
+        e = Command()
+        e.header = Header()
+        e.command = "C_STOP"
+        e.sessionID = '0'
+        pubC.publish(e)
+        print e.command
+        oneclick = False
+
+    if msg.buttons[14] == 1:
+        e = Command()
+        e.header = Header()
+        e.command = "C_SLEEP"
+        e.sessionID = '0'
+        pubC.publish(e)
+        print e.command
+        oneclick = False
+
+    if msg.buttons[7] == 1:  #left
+        e = Command()
+        e.header = Header()
+        e.command = "F_ASR_OFF"
+        e.sessionID = '0'
+        pubC.publish(e)
+        print e.command
+        oneclick = False
+
+    if msg.buttons[5] == 1:  #right
+        e = Command()
+        e.header = Header()
+        e.command = "F_ASR_ON"
+        e.sessionID = '0'
+        pubC.publish(e)
+        print e.command
+        oneclick = False
+
+    if msg.buttons[4] == 1:  #up
+        e = Command()
+        e.header = Header()
+        e.command = "F_GESTURE_ON"
+        e.sessionID = '0'
+        pubC.publish(e)
+        print e.command
+        oneclick = False
+
+    if msg.buttons[6] == 1:  #down
+        e = Command()
+        e.header = Header()
+        e.command = "F_GESTURE_OFF"
+        e.sessionID = '0'
+        pubC.publish(e)
+        print e.command
+        oneclick = False
+
+
+
+
+def main(args):        
+    rospy.init_node('Joybutton', anonymous=False)
+    
+    sub = rospy.Subscriber("/joy", Joy, joyCallback )
+    rospy.spin()
+    
+    
+if __name__ == "__main__":        
+    main(sys.argv)
