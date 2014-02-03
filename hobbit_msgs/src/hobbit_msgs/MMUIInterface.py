b9194adac02fdb8163667493a6fe4f41cf72a49a
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-02-03 16:51:03 +0000

    MMUIInterface is now pep8 compatible
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@6289 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_msgs/src/hobbit_msgs/MMUIInterface.py
+++ hobbit_msgs/src/hobbit_msgs/MMUIInterface.py
@@ -9,15 +9,14 @@
 
 
 PKG = 'hobbit_msgs'
-import roslib; roslib.load_manifest(PKG)
+import roslib
+roslib.load_manifest(PKG)
 import rospy
 from hobbit_msgs.msg import Command, Status
 from std_msgs.msg import String, Header
 #from hobbit_msgs.msg import Command, Status, Event, Parameter
 from hobbit_msgs.srv import Request, RequestRequest
 from hobbit_msgs.msg import Parameter
-
-
 
 
 class MMUIInterface:
@@ -27,92 +26,86 @@ class MMUIInterface:
         #self.stopSub = rospy.Subscriber("/Hobbit/Cancel", String, self.processStop, queue_size=1)
         self.dostop = False
 
-
     def processStop(self, msg):
-        self.dostop = True  #has to be implemented => ISTU directly on MMUI
+        self.dostop = True  # has to be implemented => ISTU directly on MMUI
 
-
-
-
-    def showMMUI_Info(self, text, wait = "1", prm = "", prm2 = "", prm3 = ""):
+    def showMMUI_Info(self, text, wait="1", prm="", prm2="", prm3=""):
         parr = []
-        p = Parameter('type','D_PLAIN')
+        p = Parameter('type', 'D_PLAIN')
         parr.append(p)
-        p = Parameter('text',text)
+        p = Parameter('text', text)
         parr.append(p)
-        p = Parameter('speak',text)
+        p = Parameter('speak', text)
         parr.append(p)
         p = Parameter('Timeout', '15')
         parr.append(p)
-        p = Parameter('Repetitions','3')
+        p = Parameter('Repetitions', '3')
         parr.append(p)
-        p = Parameter('wait',wait)
+        p = Parameter('wait', wait)
         parr.append(p)
         if prm != "":
-          p = Parameter('parameter',prm)
-          parr.append(p)
+            p = Parameter('parameter', prm)
+            parr.append(p)
         if prm2 != "":
-          p = Parameter('parameter',prm2)
-          parr.append(p)
+            p = Parameter('parameter', prm2)
+            parr.append(p)
         if prm3 != "":
-          p = Parameter('parameter',prm3)
-          parr.append(p)
-        r = self.callMMUIService('0','create',parr)
+            p = Parameter('parameter', prm3)
+            parr.append(p)
+        return self.callMMUIService('0', 'create', parr)
 
-
-    def showMMUI_OK(self, text, prm = ""):
+    def showMMUI_OK(self, text, prm=""):
         parr = []
-        p = Parameter('type','D_OK')
+        p = Parameter('type', 'D_OK')
         parr.append(p)
-        p = Parameter('text',text)
+        p = Parameter('text', text)
         parr.append(p)
-        p = Parameter('speak',text)
+        p = Parameter('speak', text)
         parr.append(p)
-        p = Parameter('Repetitions','3')
+        p = Parameter('Repetitions', '3')
         parr.append(p)
-        p = Parameter('Timeout',"15")
+        p = Parameter('Timeout', "15")
         parr.append(p)
         if prm != "":
-          p = Parameter('parameter',prm)
-          parr.append(p)
-        return self.callMMUIService('0','create',parr)
+            p = Parameter('parameter', prm)
+            parr.append(p)
+        return self.callMMUIService('0', 'create', parr)
 
-    def showMMUI_YESNO(self, text, prm = ""):
+    def showMMUI_YESNO(self, text, prm=""):
         parr = []
-        p = Parameter('type','D_YES_NO')
+        p = Parameter('type', 'D_YES_NO')
         parr.append(p)
-        p = Parameter('text',text)
+        p = Parameter('text', text)
         parr.append(p)
-        p = Parameter('speak',text)
+        p = Parameter('speak', text)
         parr.append(p)
-        p = Parameter('Repetitions',"3")
+        p = Parameter('Repetitions', "3")
         parr.append(p)
-        p = Parameter('Timeout',"15")
+        p = Parameter('Timeout', "15")
         parr.append(p)
         if prm != "":
-          p = Parameter('parameter',prm)
-          parr.append(p)
-        return self.callMMUIService('0','create',parr)
+            p = Parameter('parameter', prm)
+            parr.append(p)
+        return self.callMMUIService('0', 'create', parr)
 
     def sendMMUI_Function(self, f):
         parr = []
-        p = Parameter('type',f)
+        p = Parameter('type', f)
         parr.append(p)
-        return self.callMMUIService('0','create',parr)
+        return self.callMMUIService('0', 'create', parr)
 
-
-    def callMMUIService(self,sessionid, txt, params):
+    def callMMUIService(self, sessionid, txt, params):
         rospy.wait_for_service('/MMUI')
         servicecall = rospy.ServiceProxy('/MMUI', Request)
         try:
             h = Header()
             h.stamp = rospy.Time.now()
-            req = RequestRequest(h,sessionid,txt,params)
+            req = RequestRequest(h, sessionid, txt, params)
             resp = servicecall(req)
             return resp
         except rospy.ServiceException, e:
-          print "Service did not process request: %s"%str(e)
-          return None
+            print "Service did not process request: %s" % str(e)
+            return None
 
 
             #self.showMMUI_Info('T_CF_FIRST_GRASP_FAILED_WILL_TRY_AGAIN', "1")
