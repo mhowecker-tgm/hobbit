0c5d46075b4bfc67e1dc77cc4e5b46cef6ef2cb4
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-02-13 09:30:33 +0000

    adding startup scripts for the pt2 hobbit
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@6363 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

---
+++ hobbit_launch/launch/startupHobbitPT2.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#This script is called from ~/.config/autostart/hobbit.desktop 
+#and ( for now ) brings up all the nodes / etc required for the hobbit robot to function
+
+STARTDIR=`pwd`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+#cd to the directory where the script lies at
+
+#env > ~/startMsg.txt
+
+#Prohibit screen power down after 10mins
+xset s off&
+#Move mouse out of fov
+xdotool mousemove 1024 1024&
+
+#Display knight-rider for head :P
+animate /home/hobbit/Pictures/knight.gif&
+
+#Make sure we have our stuff sourced
+source /home/hobbit/.bashrc
+source /opt/ros/hobbit_hydro/devel/setup.bash
+
+roscore&
+sleep 3
+
+#Start Head ( and bring it to level )
+roslaunch head startup.launch&
+#Start Base
+rosrun mobile_platform mobile_platform&
+#Bring up web interface ( port 8080 )
+/opt/ros/hobbit_hydro/src/web_interface/scripts/startWebInterface.sh&
+#Start up vision acquisition etc
+/opt/ros/hobbit_hydro/src/rgbd_acquisition/scripts/startFORTHStuff.sh&
+
+#Give some time for the first camera to come online
+sleep 2
+
+#Start bottom camera
+roslaunch openni2_launch openni2.launch camera:=basecam depth_registration:=true&
+
+
+#After everything is done , bring had to up position
+rostopic pub -1 /HeadMove std_msgs/String 'up' 
+
+
+cd $STARTDIR
+
+exit 0
+
