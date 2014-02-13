25e311a15d1f01180d01d24f44bb2083f7e401c7
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-02-13 09:09:02 +0000

    adding a script to bring up the web interface on its own screen session
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@6361 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

---
+++ web_interface/scripts/startWebInterface.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ 
+#Script to start a screen session of web_interface
+
+echo "Starting up web_interface"
+screen -d -m -S "web_interface" /bin/bash -c "source /home/hobbit/.bashrc && source /opt/ros/hobbit_hydro/devel/setup.bash && roslaunch web_interface web_interface.launch"
+ 
+exit 0
+
+
+
