917d2b2958742657c692a391a1a7447ea041bbd4
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-02-14 16:12:59 +0000

    ...
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@6439 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- rgbd_acquisition/launch/startHobbitTopCamera.sh
+++ rgbd_acquisition/launch/startHobbitTopCamera.sh
@@ -42,9 +42,9 @@
 
     <!-- Produce a disparity image  -->
     <node pkg="nodelet" type="nodelet" name="disparityBroadcaster" ns="$(arg camera)" args="load depth_image_proc/disparity rgbd_nodelet_manager --no-bond" output="screen">
-     <remap from="left/image_rect" to="/$(arg camera)/depth_registered/image_rect"/>
+     <remap from="left/image_rect" to="/$(arg camera)/depth_registered/image_rect_m"/>
      <remap from="right/camera_info" to="/$(arg camera)/rgb/camera_info"/>
-     <remap from="left/disparity" to="/$(arg camera)/depth_registered/disparityLoL"/>
+     <remap from="left/disparity" to="/$(arg camera)/depth_registered/disparity"/>
     </node>
 
 
