87b924dbaec87312ea8539bf50402558e5bb66d6
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-02-14 15:11:32 +0000

    ...
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@6425 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- rgbd_acquisition/scripts/startFORTHStuff.sh
+++ rgbd_acquisition/scripts/startFORTHStuff.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-echo "Starting up rgbd_acquisition"
-screen -d -m -S "rgbd_acquisition" /bin/bash -c "source /home/hobbit/.bashrc && source /opt/ros/hobbit_hydro/devel/setup.bash && roslaunch rgbd_acquisition startHobbitTopCamera.sh"
+sleep 5
+echo "TODO : Lets try to see if everything is broadcasting"
+rostopic list
+
+ISITUP="grep"
+
+while [ -n `screen -ls | grep rgbd_acquisition` ]
+ echo "Trying to start-up rgbd_acquisition"
+ screen -d -m -S "rgbd_acquisition" /bin/bash -c "source /home/hobbit/.bashrc && source /opt/ros/hobbit_hydro/devel/setup.bash && roslaunch rgbd_acquisition startHobbitTopCamera.sh"
+ sleep 10
+done
+
+
+
 echo "Starting up hand_gestures"
 screen -d -m -S "hand_gestures" /bin/bash -c "source /home/hobbit/.bashrc && source /opt/ros/hobbit_hydro/devel/setup.bash && roslaunch hand_gestures hand_gestures.launch"
 echo "Starting up face_detection"
