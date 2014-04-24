4d8674dc515922674cec7fb5ecc6d9ded03cbf49
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-04-24 10:15:10 +0000

    User initiated emergency is finished..
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@6768 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

---
+++ hobbit_smach/src/Emergency/emergency_user.py
@@ -0,0 +1,390 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+PKG = 'hobbit_smach'
+NAME = 'emergency_user'
+DEBUG = True
+
+import roslib
+roslib.load_manifest(PKG)
+import rospy
+#import smach
+#import uashh_smach.util as util
+import uashh_smach.platform.move_base as move_base
+
+from std_msgs.msg import String
+from hobbit_msgs.msg import GeneralHobbitAction,\
+    Event
+#    Event, LocateUserAction, LocateUserGoal,\
+#    ApproachUserAction, ApproachUserGoal
+from hobbit_msgs.srv import GetCoordinates
+from smach_ros import ActionServerWrapper, \
+    IntrospectionServer, ServiceState, SimpleActionState
+from smach import StateMachine, State, cb_interface
+from hobbit_user_interaction import HobbitMMUI, HobbitEmotions
+import hobbit_smach.sos_call_import as sos_call
+from datetime import datetime, time
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+    def disable(self):
+        self.HEADER = ''
+        self.OKBLUE = ''
+        self.OKGREEN = ''
+        self.WARNING = ''
+        self.FAIL = ''
+        self.ENDC = ''
+
+
+class Init(State):
+    """Class to initialize certain parameters"""
+    def __init__(self):
+        State.__init__(
+            self,
+            outcomes=['succeeded', 'canceled'],
+            input_keys=['command'], output_keys=['social_role'])
+        self.pub_head = rospy.Publisher('HeadMove', String)
+        self.pub_obstacle = rospy.Publisher('/headcam/active', String)
+
+    def execute(self, ud):
+        self.pub_head.publish('down')
+        self.pub_obstacle.publish('active')
+        if rospy.has_param('/hobbit/social_role'):
+            ud.social_role = rospy.get_param('/hobbit/social_role')
+        return 'succeeded'
+
+
+class Dummy(State):
+    """
+    Class for setting the result message and clean up persistent variables
+    """
+    def __init__(self):
+        State.__init__(
+            self,
+            outcomes=['succeeded', 'preempted', 'failed'],
+            input_keys=['command'],
+            output_keys=['result', 'command']
+        )
+        self.pub_face = rospy.Publisher('/Hobbit/Emoticon', String)
+
+    def execute(self, ud):
+        self.pub_face.publish('EMO_SAD')
+        #ud.result = String('')
+        #rospy.sleep(2.0)
+        return 'succeeded'
+
+
+class CallCheck(State):
+    """
+    Class which checks from where the reminder scenario was called.
+    command: The user pressed the away/break button.
+    event: A calendar entry triggered the execution.
+    """
+    def __init__(self):
+        State.__init__(self,
+                       outcomes=['command', 'event', 'preempted', 'failure'],
+                       input_keys=['command', 'parameters'],
+                       output_keys=['question', 'timeframe']
+                       )
+
+    def execute(self, ud):
+        if self.preempt_requested():
+            ud.result = String('preempted')
+            return 'preempted'
+        print(ud.parameters)
+        print(ud.command)
+        if ud.parameters[0].data.lower() == 'command':
+            # TODO: Change hardcoded question to one from the translation pack
+            ud.question = 'Do you want to see the appointments \
+            for the next day?'
+            ud.timeframe = '24:00'
+            return 'command'
+        elif ud.parameters[0].data.lower() == 'event':
+            ud.timeframe = '03:00'
+            return 'event'
+        else:
+            rospy.loginfo(
+                'Unknown type in GeneralHobbitAction: %s' % ud.command.data)
+            return 'failure'
+
+
+class CleanUp(State):
+    """
+    Class for setting the result message and clean up persistent variables
+    """
+    def __init__(self):
+        State.__init__(
+            self,
+            outcomes=['succeeded'],
+            input_keys=['command'],
+            output_keys=['result', 'command']
+        )
+        self.pub_face = rospy.Publisher('/Hobbit/Emoticon', String)
+
+    def execute(self, ud):
+        self.pub_face.publish('EMO_SAD')
+        ud.result = String('')
+        return 'succeeded'
+
+
+class SetSuccess(State):
+    """
+    Class for setting the success message in the actionlib result \
+        and clean up of persistent variables
+    """
+    def __init__(self):
+        State.__init__(
+            self,
+            outcomes=['succeeded', 'preempted'],
+            output_keys=['result']
+        )
+        self.pub = rospy.Publisher('/DiscreteMotionCmd', String)
+        self.pub_face = rospy.Publisher('/Hobbit/Emoticon', String)
+
+    def execute(self, ud):
+        self.pub_face.publish('EMO_HAPPY')
+        self.pub.publish('Stop')
+        if self.preempt_requested():
+            ud.result = String('preempted')
+            self.service_preempt()
+            return 'preempted'
+        ud.result = String('success')
+        return 'succeeded'
+
+
+class SetFailure(State):
+    """
+    Class for setting the failure message in the actionlib result \
+        and clean up of persistent variables
+    """
+    def __init__(self):
+        State.__init__(
+            self,
+            outcomes=['succeeded', 'preempted'],
+            output_keys=['result']
+        )
+        self.pub = rospy.Publisher('/DiscreteMotionCmd', String)
+        self.pub_face = rospy.Publisher('/Hobbit/Emoticon', String)
+
+    def execute(self, ud):
+        self.pub_face.publish('EMO_SAD')
+        self.pub.publish('Stop')
+        if self.preempt_requested():
+            ud.result = String('preempted')
+            self.service_preempt()
+            return 'preempted'
+        ud.result = String('failure')
+        return 'succeeded'
+
+
+class TimeCheck(State):
+    """
+    Class to check if we were activated during the night.
+    """
+    def __init__(self):
+        State.__init__(
+            self,
+            outcomes=['day', 'night', 'canceled'],
+            input_keys=['night'],
+            output_keys=['night'])
+        self.sleep_time = '22:00'
+        self.wakeup_time = '06:30'
+
+    def execute(self, ud):
+        if rospy.has_param('sleep_time') and rospy.has_param('wakeup_time'):
+            self.sleep_time = rospy.get_param('sleep_time')
+            self.wakeup_time = rospy.get_param('wakeup_time')
+
+        wake = self.wakeup_time.split(':')
+        sleep = self.sleep_time.split(':')
+        now = datetime.now()
+        if time(int(wake[0]), int(wake[1]))\
+                <= now.time()\
+                <= time(int(sleep[0]), int(sleep[1])):
+            print('yes, within the interval')
+            #ud.night = False
+            return 'day'
+        else:
+            #ud.night = True
+            return 'night'
+
+
+class ActivateLights(State):
+    """
+    Class to activate the ligths in the users appartement during the night
+    """
+    def __init__(self):
+        State.__init__(
+            self,
+            outcomes=['succeeded', 'preempted', 'aborted']
+        )
+
+    def execute(self, ud):
+        if self.preempt_requested():
+            ud.result = String('preempted')
+            self.service_preempt()
+            return 'preempted'
+        # TODO: Call tcp client to communicate with the light switch
+        return 'succeeded'
+
+
+@cb_interface(input_keys=['room_name'])
+def set_nav_goal_cb(userdata, request):
+    nav_request = GetCoordinates().Request
+    nav_request.room_name = String(userdata.room_name)
+    return nav_request
+
+
+def main():
+    rospy.init_node(NAME)
+
+    emus_sm = StateMachine(
+        outcomes=['succeeded', 'failure', 'preempted'],
+        input_keys=['command', 'previous_state', 'parameters'],
+        output_keys=['result'])
+
+    emus_sm.userdata.result = String('started')
+    emus_sm.userdata.detection = False
+
+    with emus_sm:
+        StateMachine.add(
+            'INIT',
+            Init(),
+            transitions={'succeeded': 'TIME_CHECK',
+                         'canceled': 'CLEAN_UP'})
+        StateMachine.add(
+            'TIME_CHECK',
+            TimeCheck(),
+            transitions={'day': 'EMO_CONCERNED',
+                         'night': 'ACTIVATE_LIGHTS',
+                         'canceled': 'CLEAN_UP',
+                         #'preempted': 'preempted'
+                         }
+        )
+        StateMachine.add(
+            'ACTIVATE_LIGHTS',
+            Dummy(),
+            #ActivateLights(),
+            transitions={'succeeded': 'MMUI_CONFIRM_DoYouNeedHelp',
+                         'failed': 'MMUI_CONFIRM_DoYouNeedHelp',
+                         'preempted': 'preempted'}
+        )
+        StateMachine.add(
+            'EMO_CONCERNED',
+            HobbitEmotions.ShowEmotions(emotion='CONCERNED', emo_time=0),
+            transitions={'succeeded': 'MMUI_CONFIRM_DoYouNeedHelp',
+                         'preempted': 'preempted',
+                         'failed': 'SET_FAILURE'}
+        )
+        # Why should Hobbit look for the user if the command was givven on
+        # the touchscreen, by voice or by gesture.
+        # This means the user is at least in front or very close to hoobit
+        #StateMachine.add(
+        #    'LOCATE_USER',
+        #    SimpleActionState(
+        #        'locate_user',
+        #        LocateUserAction,
+        #        goal=LocateUserGoal(command=String('locateUser'))),
+        #    {'succeeded': 'APPROACH_USER',
+        #     'aborted': 'CLEAN_UP',
+        #     'preempted': 'CLEAN_UP'}
+        #)
+        #StateMachine.add(
+        #    'APPROACH_USER',
+        #    SimpleActionState(
+        #        'approach_user',
+        #        ApproachUserAction,
+        #        goal=ApproachUserGoal(command=String('approachUser'))),
+        #    {'succeeded': 'MMUI_CONFIRM',
+        #     'aborted': 'CLEAN_UP',
+        #     'preempted': 'CLEAN_UP'}
+        #)
+        StateMachine.add(
+            'MMUI_CONFIRM_DoYouNeedHelp',
+            HobbitMMUI.AskYesNo(question='DoYouNeedHelp'),
+            transitions={'yes': 'MMUI_CONFIRM_ShallICallHelp',
+                         'no': 'succeeded',
+                         'timeout': 'MMUI_CONFIRM_DoYouNeedHelp',
+                         '3times': 'EMERGENCY_CALL',
+                         'failed': 'SET_FAILURE'}
+        )
+        StateMachine.add(
+            'MMUI_CONFIRM_ShallICallHelp',
+            HobbitMMUI.AskYesNo(question='T_HM_StayCalmShallICallHelp'),
+            transitions={'yes': 'EMERGENCY_CALL',
+                         'no': 'MMUI_SAY_YouCanGetHelpAnytime',
+                         'timeout': 'MMUI_CONFIRM_ShallICallHelp',
+                         '3times': 'EMERGENCY_CALL',
+                         'failed': 'SET_FAILURE'}
+        )
+        StateMachine.add(
+            'MMUI_SAY_YouCanGetHelpAnytime',
+            HobbitMMUI.ShowInfo(info='T_HM_YouCanGetHelpAnytime'),
+            transitions={'succeeded': 'EMO_NEUTRAL',
+                         'preempted': 'preempted',
+                         'failed': 'SET_FAILURE'}
+        )
+        StateMachine.add(
+            'EMERGENCY_CALL',
+            sos_call.get_call_sos(),
+            transitions={'succeeded': 'SET_SUCCESS',
+                         'aborted': 'SET_FAILURE',
+                         'preempted': 'preempted'}
+        )
+        StateMachine.add(
+            'EMO_NEUTRAL',
+            HobbitEmotions.ShowEmotions(emotion='NEUTRAL', emo_time=0),
+            transitions={'succeeded': 'MMUI_MAIN_MENU',
+                         'failed': 'SET_FAILURE',
+                         'preempted': 'preempted'}
+        )
+        StateMachine.add(
+            'MMUI_MAIN_MENU',
+            HobbitMMUI.ShowMenu(menu='MAIN'),
+            transitions={'succeeded': 'SET_SUCCESS',
+                         'preempted': 'preempted',
+                         'failed': 'SET_FAILURE'}
+        )
+        StateMachine.add(
+            'SET_SUCCESS',
+            SetSuccess(),
+            transitions={'succeeded': 'succeeded',
+                         'preempted': 'CLEAN_UP'}
+        )
+        StateMachine.add(
+            'SET_FAILURE',
+            SetFailure(),
+            transitions={'succeeded': 'failure',
+                         'preempted': 'preempted'}
+        )
+        StateMachine.add(
+            'CLEAN_UP',
+            CleanUp(),
+            transitions={'succeeded': 'preempted'})
+
+    asw = ActionServerWrapper(
+        'emergency_user',
+        GeneralHobbitAction,
+        emus_sm,
+        ['succeeded'], ['failure'], ['preempted'],
+        result_slots_map={'result': 'result'},
+        goal_slots_map={'command': 'command',
+                        'previous_state': 'previous_state',
+                        'parameters': 'parameters'}
+    )
+
+    sis = IntrospectionServer('smach_server', emus_sm, '/HOBBIT/emus_sm_ROOT')
+    sis.start()
+    asw.run_server()
+    rospy.spin()
+    sis.stop()
+
+if __name__ == '__main__':
+    main()
---
+++ hobbit_smach/src/hobbit_smach/sos_call_import.py
@@ -0,0 +1,339 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+PKG = 'hobbit_smach'
+
+import roslib
+roslib.load_manifest(PKG)
+import rospy
+
+from hobbit_user_interaction import HobbitMMUI, HobbitEmotions
+import hobbit_smach.hobbit_move as hobbit_move
+
+from std_msgs.msg import String
+from hobbit_msgs.msg import GeneralHobbitAction, Event
+from smach_ros import ActionServerWrapper, \
+    IntrospectionServer
+from smach import StateMachine, State, Sequence
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+    def disable(self):
+        self.HEADER = ''
+        self.OKBLUE = ''
+        self.OKGREEN = ''
+        self.WARNING = ''
+        self.FAIL = ''
+        self.ENDC = ''
+
+
+class Init(State):
+
+    """
+    Class to initialize certain parameters
+    """
+
+    def __init__(self):
+        State.__init__(
+            self,
+            outcomes=['succeeded', 'aborted', 'preempted'],
+            output_keys=['social_role'])
+
+    def execute(self, ud):
+        if rospy.has_param('/hobbit/social_role'):
+            ud.social_role = rospy.get_param('/hobbit/social_role')
+        else:
+            pass
+        return 'succeeded'
+
+
+class CleanUp(State):
+
+    """
+    Class for setting the result message and clean up persistent variables
+    """
+
+    def __init__(self):
+        State.__init__(
+            self,
+            outcomes=['succeeded'],
+            input_keys=['command', 'visited_places'],
+            output_keys=['result', 'command', 'visited_places'])
+        self.pub_face = rospy.Publisher('/Hobbit/Emoticon', String)
+
+    def execute(self, ud):
+        self.pub_face.publish('EMO_SAD')
+        ud.visited_places = []
+        ud.result = String('user not detected')
+        return 'succeeded'
+
+
+class SetSuccess(State):
+
+    """
+    Class for setting the success message in the actionlib result and clean
+    up of persistent variables
+    """
+
+    def __init__(self):
+        State.__init__(
+            self,
+            outcomes=['succeeded', 'preempted'],
+            output_keys=['result', 'visited_places'])
+        self.pub = rospy.Publisher('/DiscreteMotionCmd', String)
+        self.pub_face = rospy.Publisher('/Hobbit/Emoticon', String)
+
+    def execute(self, ud):
+        ud.visited_places = []
+        self.pub_face.publish('EMO_HAPPY')
+        self.pub.publish('Stop')
+        if self.preempt_requested():
+            ud.result = String('preempted')
+            self.service_preempt()
+            return 'preempted'
+        ud.result = String('user detected')
+        return 'succeeded'
+
+
+class Dummy(State):
+
+    """
+    Class for setting the result message and clean up persistent variables
+    """
+
+    def __init__(self):
+        State.__init__(
+            self,
+            outcomes=['succeeded', 'failed', 'preempted'],
+            input_keys=['command'],
+            output_keys=['result', 'command']
+        )
+
+    def execute(self, ud):
+        return 'succeeded'
+
+
+class CheckCaller(State):
+
+    """
+    What emergency called SOS?
+    """
+
+    def __init__(self):
+        State.__init__(
+            self,
+            outcomes=['succeeded', 'failed', 'preempted', 'move_to_dock'],
+            input_keys=['parameters']
+        )
+
+    def execute(self, ud):
+        if ud.parameters[0].data == 'user_not_detected':
+            return 'move_to_dock'
+        else:
+            return 'succeeded'
+
+
+class FailCounter(State):
+
+    """
+    Class for setting the result message and clean up persistent variables
+    """
+
+    def __init__(self):
+        State.__init__(
+            self,
+            outcomes=['fail1', 'fail2', 'fail3', 'loop'],
+            input_keys=['parameters'],
+            output_keys=['result', 'command']
+        )
+        self.loop = False
+        self.fail = 1
+
+    def execute(self, ud):
+        if self.fail == 3:
+            self.fail = 1
+            if ud.parameters[0].data == 'user_initiated':
+                print(bcolors.OKGREEN +
+                      'user_initiated' +
+                      bcolors.ENDC)
+                if not self.loop:
+                    self.loop = True
+                    return 'loop'
+                else:
+                    self.loop = False
+                    return 'fail3'
+            else:
+                return 'fail3'
+        elif self.fail == 2:
+            self.fail += 1
+            return 'fail2'
+        elif self.fail == 1:
+            self.fail += 1
+            return 'fail1'
+        return 'fail3'
+
+
+def callstate_cb(msg, ud):
+    ud.call_state = msg.command
+    print(msg.command)
+    if msg.event == 'E_CALLCONFIRMED':
+        return True
+    else:
+        return False
+
+
+def get_call_sos():
+    sos_sm = StateMachine(
+        outcomes=['succeeded', 'aborted', 'preempted'],
+        input_keys=['command', 'parameters'],
+        output_keys=['result'])
+
+    sos_sm.userdata.result = String('started')
+
+    seq = Sequence(
+        outcomes=['succeeded', 'preempted', 'failed'],
+        connector_outcome='succeeded'
+    )
+
+    with seq:
+        Sequence.add(
+            'WAIT_FOR_MMUI',
+            HobbitMMUI.WaitforSoundEnd('/Event', Event),
+            transitions={'aborted': 'WAIT_FOR_MMUI'})
+        Sequence.add('START_CALL', HobbitMMUI.CallEmergency())
+        Sequence.add(
+            'CHECK_CALL_STATE',
+            HobbitMMUI.WaitforConfirmedCall(
+                '/Event', Event, output_keys=['call_state']),
+            transitions={'aborted': 'CHECK_CALL_STATE'}
+        )
+        Sequence.add(
+            'CALL_DECISION',
+            HobbitMMUI.CallDecison(),
+            transitions={'stop': 'END_CALL',
+                         'ended': 'failed'}
+        )
+        Sequence.add(
+            'CHECK_CALL_ENDED',
+            HobbitMMUI.WaitforEndedCall(
+                '/Event', Event, output_keys=['call_state']),
+            transitions={'aborted': 'CHECK_CALL_ENDED'}
+        )
+        Sequence.add('END_CALL', HobbitMMUI.CallEnded())
+
+    with sos_sm:
+        StateMachine.add(
+            'INIT',
+            Init(),
+            transitions={'succeeded': 'SAY_T_HM_IWillCallFirstPersonInList',
+                         'preempted': 'preempted'}
+        )
+        StateMachine.add(
+            'CHECK_CALLER',
+            CheckCaller(),
+            transitions={'succeeded': 'MMUI_MAIN_MENU',
+                         'failed': 'aborted',
+                         'preempted': 'preempted',
+                         'move_to_dock': 'MOVE_TO_DOCK'}
+        )
+        StateMachine.add(
+            'SEQ',
+            seq,
+            transitions={'succeeded': 'SAY_IAmWithYou',
+                         'failed': 'FAIL_COUNTER'}
+        )
+        StateMachine.add(
+            'MMUI_MAIN_MENU',
+            HobbitMMUI.ShowMenu(menu='MAIN'),
+            transitions={'succeeded': 'succeeded',
+                         'failed': 'aborted',
+                         'preempted': 'preempted'}
+        )
+        StateMachine.add(
+            'FAIL_COUNTER',
+            FailCounter(),
+            transitions={'loop': 'SAY_T_HM_NoAnswersIWillCallFirstPersonAgain',
+                         'fail1': 'SAY_T_HM_IWillCallSecondPersonInList',
+                         'fail2': 'SAY_T_HM_IWillCallThirdPersonInList',
+                         'fail3': 'SAY_NoAnswersSayOrPressHelpAgainToTryAgain'}
+        )
+        StateMachine.add(
+            'SAY_T_HM_IWillCallFirstPersonInList',
+            HobbitMMUI.ShowInfo(info='IWillCallFirstPersonInList'),
+            transitions={'succeeded': 'SEQ',
+                         'failed': 'SEQ',
+                         'preempted': 'preempted'}
+        )
+        StateMachine.add(
+            'SAY_T_HM_IWillCallSecondPersonInList',
+            HobbitMMUI.ShowInfo(info='IWillCallSecondPersonInList'),
+            transitions={'succeeded': 'SEQ',
+                         'failed': 'SEQ',
+                         'preempted': 'preempted'}
+        )
+        StateMachine.add(
+            'SAY_T_HM_IWillCallThirdPersonInList',
+            HobbitMMUI.ShowInfo(info='IWillCallThirdPersonInList'),
+            transitions={'succeeded': 'SEQ',
+                         'failed': 'SEQ',
+                         'preempted': 'preempted'}
+        )
+        StateMachine.add(
+            'SAY_T_HM_NoAnswersIWillCallFirstPersonAgain',
+            HobbitMMUI.ShowInfo(info='NoAnswersIWillCallFirstPersonAgain'),
+            transitions={'succeeded': 'SEQ',
+                         'failed': 'SEQ',
+                         'preempted': 'preempted'}
+        )
+        StateMachine.add(
+            'SAY_NoAnswersSayOrPressHelpAgainToTryAgain',
+            HobbitMMUI.ShowInfo(info='NoAnswersSayOrPressHelpAgainToTryAgain'),
+            transitions={'succeeded': 'WAIT_FOR_MMUI',
+                         'failed': 'aborted',
+                         'preempted': 'preempted'}
+        )
+        StateMachine.add(
+            'WAIT_FOR_MMUI',
+            HobbitMMUI.WaitforSoundEnd('/Event', Event),
+            transitions={'aborted': 'WAIT_FOR_MMUI',
+                         'succeeded': 'SAY_IAmWithYou'}
+        )
+        StateMachine.add(
+            'EMO_NEUTRAL',
+            HobbitEmotions.ShowEmotions(emotion='NEUTRAL', emo_time=4),
+            transitions={'succeeded': 'END_CALL',
+                         'failed': 'aborted',
+                         'preempted': 'preempted'}
+        )
+        StateMachine.add(
+            'END_CALL',
+            HobbitMMUI.CallEnded(),
+            transitions={'succeeded': 'CHECK_CALLER',
+                         'failed': 'aborted',
+                         'preempted': 'preempted'}
+        )
+        StateMachine.add(
+            'MOVE_TO_DOCK',
+            hobbit_move.goToPosition(room=None, place='dock'),
+            transitions={'succeeded': 'MMUI_MAIN_MENU',
+                         'aborted': 'aborted',
+                         'preempted': 'preempted'}
+        )
+        StateMachine.add(
+            'SAY_IAmWithYou',
+            HobbitMMUI.ShowInfo(info='IAmWithYou'),
+            transitions={'succeeded': 'EMO_NEUTRAL',
+                         'preempted': 'preempted',
+                         'failed': 'EMO_NEUTRAL'}
+        )
+    return sos_sm
+
+if __name__ == '__main__':
+    print('You should import this file, not execute it.')
