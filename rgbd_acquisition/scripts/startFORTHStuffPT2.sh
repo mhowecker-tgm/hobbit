a65cdc559acf831395d0ef77a274397f145c60a2
paloma.puente <paloma.puente@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-06-02 11:59:41 +0000

    ...
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@6970 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- rgbd_acquisition/scripts/startFORTHStuffPT2.sh
+++ rgbd_acquisition/scripts/startFORTHStuffPT2.sh
@@ -2,7 +2,7 @@
 
 #Quick Settings
 HOBBITDIR="/opt/ros/hobbit_hydro/"
-SLEEP_TIME_IN_SECONDS_BETWEEN_RETRIES=10
+SLEEP_TIME_IN_SECONDS_BETWEEN_RETRIES=15
 MAX_RETRIES=10
  
  
@@ -15,10 +15,10 @@ then
  HOBBITDIR="~/Documents/Programming/FORTH/Hobbit/hobbit_hydro/"
 fi
 
-
+echo "NewLog" > ~/debug.txt 
 #Try to bring up our node for the first time!
 echo "Trying to bring RGBD Node up with a first try"
-screen -d -m -S "rgbd_acquisition" /bin/bash -c "source ~/.bashrc && source $HOBBITDIR/devel/setup.bash && roslaunch rgbd_acquisition startHobbitTopCamera.sh"
+screen -d -m -S "rgbd_acquisition" /bin/bash -c "source ~/.bashrc && source $HOBBITDIR/devel/setup.bash && roslaunch rgbd_acquisition startHobbitTopCameraPT2.sh &>> ~/debug.txt"
 sleep $SLEEP_TIME_IN_SECONDS_BETWEEN_RETRIES
 
 
@@ -27,11 +27,12 @@ i=2
 ISRGBDUP=`rostopic list | grep /headcam/rgb/image_rect_color/compressed`
 while [ -z "$ISRGBDUP" ]
 do 
+ screen -S "rgbd_acquisition" -X quit
  echo "RGBD node not started yet $i/$MAX_RETRIES ( $ISRGBDUP )"
  sleep $SLEEP_TIME_IN_SECONDS_BETWEEN_RETRIES
  
  #Do Startup Here
-  screen -d -m -S "rgbd_acquisition" /bin/bash -c "source ~/.bashrc && source $HOBBITDIR/devel/setup.bash && roslaunch rgbd_acquisition startHobbitTopCameraPT2.sh"
+  screen -d -m -S "rgbd_acquisition" /bin/bash -c "source ~/.bashrc && source $HOBBITDIR/devel/setup.bash && roslaunch rgbd_acquisition startHobbitTopCameraPT2.sh &>> ~/debug.txt"
  #Do Startup Here
  
  ISRGBDUP=`rostopic list | grep /headcam/rgb/image_rect_color/compressed`
