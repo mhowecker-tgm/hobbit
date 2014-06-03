4da185c82180d0589bb88c6e3ac5df36edaff13f
paloma.puente <paloma.puente@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-06-03 13:04:15 +0000

    usbworkaround
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@6976 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

---
+++ rgbd_acquisition/scripts/workAroundUSB.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Quick Settings
+HOBBITDIR="/opt/ros/hobbit_hydro/"  
+DEVICE_TOP="1d27/0601@1/3"
+DEVICE_BASE="1d27/0601@2/3"
+
+#Ammar's installation at FORTH may override hobbit dir
+if [ -d $HOBBITDIR ] 
+then
+ echo "Hobbit Normal root dir"
+elif [ -d ~/Documents/Programming/FORTH/Hobbit/hobbit_hydro ] 
+then
+ HOBBITDIR="~/Documents/Programming/FORTH/Hobbit/hobbit_hydro/"
+fi
+
+cd $HOBBITDIR/src/rgbd_acquisition/bin
+ 
+echo "Doing a camera powercycle workaround for maximum robustness"
+
+./Grabber -module OPENNI2 -from "$DEVICE_BASE" -to /dev/null -maxFrames 30
+./Grabber -module OPENNI2 -from "$DEVICE_TOP" -to /dev/null -maxFrames 30
+
+echo "Done " 
+exit 0
