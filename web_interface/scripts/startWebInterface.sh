410bc4c10588e36d5b16b2edabe4c7749fb85ec7
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-06-04 14:04:12 +0000

    ...
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@6993 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- web_interface/scripts/startWebInterface.sh
+++ web_interface/scripts/startWebInterface.sh
@@ -3,7 +3,7 @@
 #Script to start a screen session of web_interface
 
 echo "Starting up web_interface"
-screen -d -m -S "web_interface" /bin/bash -c "source /home/hobbit/.bashrc && source /opt/ros/hobbit_hydro/devel/setup.bash && roslaunch web_interface web_interface.launch"
+screen -d -m -S "web_interface" /bin/bash -c "source ~/.bashrc && source /opt/ros/hobbit_hydro/devel/setup.bash && roslaunch web_interface web_interface.launch"
  
 exit 0
 
