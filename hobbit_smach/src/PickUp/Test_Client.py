caf25802f2da680b2c7503e3a8977cd950b6d793
david.fischinger <david.fischinger@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-06-06 10:22:23 +0000

    added arm controller for pickup
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7039 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

---
+++ hobbit_smach/src/PickUp/Test_Client.py
@@ -0,0 +1,61 @@
+'''
+Created on 07.01.2014
+
+@author: HofmannS
+'''
+from ArmControllerClientFunctions import ArmClientFunctions
+
+ArmClient = ArmClientFunctions('192.168.2.190')
+
+''' GET Functions '''
+#print ArmClient.GetArmState()                          
+#print ArmClient.GetActualPosition()                    
+#print ArmClient.GetArmAtHomePos()                      
+#print ArmClient.GetArmAtLearningPos()                  
+#print ArmClient.GetArmAtTrayPos()                      
+#print ArmClient.GetArmAtTurntablePos()                 
+#print ArmClient.GetTurntableAtCCWPos()                 
+#print ArmClient.GetTurntableAtCWPos()                              
+#print ArmClient.GetArmHasError()                        
+#print ArmClient.GetArmHasStopped()                      
+#print ArmClient.GetArmInPositionArea()                  
+#print ArmClient.GetArmInTargetPos()                     
+#print ArmClient.GetArmIsEnabled()                       
+#print ArmClient.GetArmIsHomed()                         
+#print ArmClient.GetArmIsMoving()                        
+#print ArmClient.GetArmSoftLimitMax()                    
+#print ArmClient.GetArmSoftLimitMin()                    
+#print ArmClient.GetGripperIsClosed()                    
+
+
+''' SET Functions '''
+#print ArmClient.SetAbsolutePos(90, 0, 50, 0, 110, 0)
+#print ArmClient.SetStartMove(10) #10 Grad/Sec
+print ArmClient.SetMoveToHomePos()
+#print ArmClient.SetMoveToLearningPos()
+#print ArmClient.SetMoveToTrayPos()
+#print ArmClient.SetMoveToPreGraspFromFloorPos()
+#print ArmClient.SetStoreTurntable()
+#print ArmClient.SetTurnTurntableCW()
+#print ArmClient.SetTurnTurntableCCW()
+#print ArmClient.SetStartArmReference()
+#print ArmClient.SetDisableArm()
+#print ArmClient.SetEnableArm()
+#print ArmClient.SetOpenGripper()
+#print ArmClient.SetCloseGripper()
+#print ArmClient.SetResetArm()
+#print ArmClient.SetStopArmMove()
+
+''' For Interpolation Mode '''
+#ArmClient.SetClearPosBuffer()
+#ArmClient.SetPositionsForInterpolation(90, 86, 70, 0, 110, 0)
+#ArmClient.SetPositionsForInterpolation(90, 70, 65, 0, 110, 0)
+#ArmClient.SetPositionsForInterpolation(90, 60, 60, 0, 110, 0)
+#ArmClient.SetPositionsForInterpolation(90, 50, 60, 0, 110, 0)
+#ArmClient.SetPositionsForInterpolation(90, 40, 55, 0, 110, 0)
+#ArmClient.SetPositionsForInterpolation(90, 30, 55, 0, 110, 0)
+#ArmClient.SetPositionsForInterpolation(90, 20, 50, 0, 110, 0)
+#ArmClient.SetPositionsForInterpolation(90, 0, 50, 0, 110, 0)
+#ArmClient.SetPositionsForInterpolationReady()
+#ArmClient.SetStartInterpolation()
+
