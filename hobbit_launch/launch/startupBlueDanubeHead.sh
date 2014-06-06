36b59f26764bfb51f88b840e3d67df8e14e022cd
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-06-06 09:50:52 +0000

    reader friendly startupBlueDanube script
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7038 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_launch/launch/startupBlueDanubeHead.sh
+++ hobbit_launch/launch/startupBlueDanubeHead.sh
@@ -1,36 +1,23 @@
-#!/bin/bash
-
-
-#Hobbit PT2
-#1) starte roscore at
-#2) connect with pi: ssh pi@192.168.2.199
-#pw: pi
-#(augen starten automatisch)
-#=> strg+c , ctrl+c 
-
-#rosrun blue_owlpose owlpose.py
-
-
-#rostopic echo /tf (anschaun)
-#littledown_center
-#up
-#center
-#down
-#left
-#center
-#right
-#to_turntable
-#to_grasp
-
-#rostopic pub /head/move std_msgs/String "center_right" -1
+#!/bin/bash 
 
 HOBBITDIR="/opt/ros/hobbit_hydro/"
 screen -d -m -S "head" /bin/bash -c "source ~/.bashrc && source $HOBBITDIR/devel/setup.bash && netcat 192.168.2.199 8080"
+ 
+#Useful Commands :
+ 
+#Before anything else ( and after the robot is started one must do )
+#rostopic pub /head/cmd std_msgs/String "startup" -1
 
-#netcat 192.168.2.199 8080&
+#To Move the head you can use
+#rostopic pub /head/move std_msgs/String "center_right" -1
+#it can be moved to down_center up center down left center right to_turntable to_grasp
 
+#To restart the servos you can do
+#rostopic pub /head/cmd std_msgs/String "restart" -1
 
-
+#To change the emotion you can do 
+#rostopic pub /head/emo std_msgs/String "HAPPY" -1
+#Emotions are HAPPY VHAPPY LTIRED VTIRED CONCERNED SAD WONDERING NEUTRAL SLEEPING
 
 
 exit 0 
