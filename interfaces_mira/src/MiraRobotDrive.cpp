1d97e4a30adce1a16a354711cd50951f90f24445
paloma.puente <paloma.puente@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-06-11 09:02:50 +0000

    MiraRobotDriveprovided tf changed child to base_link, it was not working for amcl
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7075 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- interfaces_mira/src/MiraRobotDrive.cpp
+++ interfaces_mira/src/MiraRobotDrive.cpp
@@ -197,7 +197,7 @@ void MiraRobotDrive::odometry_data_callback(mira::ChannelRead<mira::robot::Odome
 	geometry_msgs::TransformStamped odom_tf;
 	odom_tf.header.stamp = odom_time;
 	odom_tf.header.frame_id = "/odom";
-	odom_tf.child_frame_id = "/base_link";
+	odom_tf.child_frame_id = "/base_link"; //originally it was "/base_footprint", did not work for amcl
 
 	odom_tf.transform.translation.x = data->value().pose.x();
 	odom_tf.transform.translation.y = data->value().pose.y();
