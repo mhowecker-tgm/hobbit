2b6228aad4e7babb1b9bda880597c8cc3f29a9ba
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-06-17 10:46:30 +0000

    emergency detector startup scripts etc
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7142 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- emergency_detector/src/main.cpp
+++ emergency_detector/src/main.cpp
@@ -22,6 +22,7 @@
 #include <sensor_msgs/CameraInfo.h>
 #include <image_transport/image_transport.h>
 
+#include "emergency_detector/SkeletonBBox.h"
 
 #if BROADCAST_HOBBIT
 #include "hobbit_msgs/Event.h"
@@ -30,9 +31,32 @@ ros::Publisher gestureEventBroadcaster;
 #endif
 
 
+#define DEFAULT_FRAME_RATE 7
+
+int rate=DEFAULT_FRAME_RATE;
+
+
+message_filters::Subscriber<sensor_msgs::Image> *rgb_img_sub;
+message_filters::Subscriber<sensor_msgs::CameraInfo> *rgb_cam_info_sub;
+message_filters::Subscriber<sensor_msgs::Image> *depth_img_sub;
+message_filters::Subscriber<sensor_msgs::CameraInfo> *depth_cam_info_sub;
+//OpenCV
+ cv::Mat rgb,depth;
+
+typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> RgbdSyncPolicy;
+
+
+
+bool first=false;
 int key = 0;
+unsigned int frameTimestamp=0;
 ros::NodeHandle * nhPtr=0;
+unsigned int paused=0;
+
+
 unsigned int emergencyDetected=0;
+float temperatureDetected=0.0;
+
 
 void broadcastEmergency(unsigned int frameNumber)
 {
@@ -71,16 +95,62 @@ bool terminate(std_srvs::Empty::Request& request, std_srvs::Empty::Response& res
 }
 
 
+bool trigger(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)
+{
+    emergencyDetected=1;
+    return true;
+}
+
 bool pause(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)
 {
+    paused=1;
     return true;
 }
 
 bool resume(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)
 {
+    paused=0;
     return true;
 }
 
+int runServicesThatNeedColorAndDepth(unsigned char * colorFrame , unsigned int colorWidth , unsigned int colorHeight ,
+                                       unsigned short * depthFrame  , unsigned int depthWidth , unsigned int depthHeight ,
+                                        void * calib ,
+                                          unsigned int frameTimestamp )
+{
+ return 0;
+}
+
+void bboxReceived(const emergency_detector::SkeletonBBox & msg)
+{
+  fprintf(stderr,"Received BBOX %f , %f , %f \n",msg.width , msg.height ,msg.depth );
+}
+
+//RGBd Callback is called every time we get a new pair of frames , it is synchronized to the main thread
+void rgbdCallbackNoCalibration(const sensor_msgs::Image::ConstPtr rgb_img_msg,
+                                 const sensor_msgs::Image::ConstPtr depth_img_msg  )
+{
+ if (paused) { return; } //If we are paused spend no time with new input
+  //A new pair of frames has arrived , copy and convert them so that they are ready
+  unsigned int colorWidth = rgb_img_msg->width;   unsigned int colorHeight = rgb_img_msg->height;
+  unsigned int depthWidth = depth_img_msg->width; unsigned int depthHeight = depth_img_msg->height;
+
+  cv_bridge::CvImageConstPtr orig_rgb_img;
+  cv_bridge::CvImageConstPtr orig_depth_img;
+  orig_rgb_img = cv_bridge::toCvCopy(rgb_img_msg, "rgb8");
+  orig_rgb_img->image.copyTo(rgb);
+  orig_depth_img = cv_bridge::toCvCopy(depth_img_msg, sensor_msgs::image_encodings::TYPE_16UC1);
+  orig_depth_img->image.copyTo(depth);
+
+  //doDrawOut();
+  runServicesThatNeedColorAndDepth((unsigned char*) rgb.data, colorWidth , colorHeight ,
+                                   (unsigned short*) depth.data ,  depthWidth , depthHeight ,
+                                     0 , frameTimestamp );
+ //After running (at least) once it is not a first run any more!
+ first = false;
+ ++frameTimestamp;
+return;
+}
 
 int main(int argc, char **argv)
 {
@@ -91,7 +161,7 @@ int main(int argc, char **argv)
 	 ROS_INFO("Initializing ROS");
 
 	 char regName[128]={0};
-	 sprintf(regName,"HandGestures%u",getpid());
+	 sprintf(regName,"EmergencyDetector%u",getpid());
 	 fprintf(stderr,"Node named %s \n",regName);
   	 ros::init(argc, argv, regName);
      ros::start();
@@ -99,10 +169,47 @@ int main(int argc, char **argv)
      ros::NodeHandle nh;
      nhPtr = &nh;
 
+     ros::NodeHandle private_node_handle_("~");
+
+     std::string name;
+     std::string fromDepthTopic;
+     std::string fromDepthTopicInfo;
+     std::string fromRGBTopic;
+     std::string fromRGBTopicInfo;
+
+
+     private_node_handle_.param("fromDepthTopic", fromDepthTopic, std::string("/headcam/depth_registered/image_rect"));
+     private_node_handle_.param("fromDepthTopicInfo", fromDepthTopicInfo, std::string("/headcam/depth_registered/camera_info"));
+     private_node_handle_.param("fromRGBTopic", fromRGBTopic, std::string("headcam/rgb/image_rect_color"));
+     private_node_handle_.param("fromRGBTopicInfo", fromRGBTopicInfo, std::string("/headcam/rgb/camera_info"));
+     private_node_handle_.param("name", name, std::string("emergency_detector"));
+     private_node_handle_.param("rate", rate , int(DEFAULT_FRAME_RATE)); //11 should me optimal  less for a little less CPU Usage
+     ros::Rate loop_rate(rate); //  hz should be our target performance
+
+
      //We advertise the services we want accessible using "rosservice call *w/e*"
      ros::ServiceServer pauseGestureRecognitionService    = nh.advertiseService("emergency_detector/pause", pause);
      ros::ServiceServer resumeGestureRecognitionService   = nh.advertiseService("emergency_detector/resume", resume);
      ros::ServiceServer stopGestureRecognitionService     = nh.advertiseService("emergency_detector/terminate", terminate);
+     ros::ServiceServer triggerGestureRecognitionService     = nh.advertiseService("emergency_detector/trigger", trigger);
+
+
+     //Make our rostopic cmaera grabber
+     message_filters::Synchronizer<RgbdSyncPolicy> *sync;
+
+	 depth_img_sub = new message_filters::Subscriber<sensor_msgs::Image>(nh,fromDepthTopic,1);
+	 depth_cam_info_sub = new message_filters::Subscriber<sensor_msgs::CameraInfo>(nh,fromDepthTopicInfo,1);
+
+	 rgb_img_sub = new  message_filters::Subscriber<sensor_msgs::Image>(nh,fromRGBTopic, 1);
+	 rgb_cam_info_sub = new message_filters::Subscriber<sensor_msgs::CameraInfo>(nh,fromRGBTopicInfo,1);
+
+     sync = new message_filters::Synchronizer<RgbdSyncPolicy>(RgbdSyncPolicy(rate), *rgb_img_sub, *depth_img_sub); //*rgb_cam_info_sub,
+	 sync->registerCallback(rgbdCallbackNoCalibration);
+
+     ros::Subscriber sub = nh.subscribe("jointsBBox",1000,bboxReceived);
+     #if BROADCAST_HOBBIT
+      gestureEventBroadcaster = nh->advertise <hobbit_msgs::Event> ("Event", 1000);
+     #endif
 
       //Create our context
       //---------------------------------------------------------------------------------------------------
@@ -113,6 +220,13 @@ int main(int argc, char **argv)
                   usleep(1000);
 		 }
 
+
+
+	   delete depth_img_sub;
+	   delete depth_cam_info_sub;
+	   delete rgb_img_sub;
+	   delete rgb_cam_info_sub;
+	   delete sync;
 	}
 	catch(std::exception &e) { ROS_ERROR("Exception: %s", e.what()); return 1; }
 	catch(...)               { ROS_ERROR("Unknown Error"); return 1; }
