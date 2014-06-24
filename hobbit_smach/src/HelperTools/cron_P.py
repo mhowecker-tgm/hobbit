a5661c202ad997db1993b4e878646f3e780156e8
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-06-24 08:27:02 +0000

    Add cronjob publishers.
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7205 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

---
+++ hobbit_smach/src/HelperTools/cron_P.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import rospy
+from hobbit_msgs.msg import Event
+from std_msgs.msg import Header
+
+def talker():
+    rospy.init_node('CRON_P', anonymous=True)
+    data = Event()
+    data.header = Header()
+    data.header.stamp = rospy.Time.now()
+    data.event = 'E_PATROL'
+    pub = rospy.Publisher('Event', Event, queue_size=10)
+    pub.publish(data)
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException: pass
