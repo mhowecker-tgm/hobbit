16d4e811ba1e99fe99e77ef23cd1d49a499becef
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-06-24 08:45:59 +0000

    cron works now.
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7207 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_smach/src/HelperTools/cron_CF.py
+++ hobbit_smach/src/HelperTools/cron_CF.py
@@ -2,6 +2,7 @@
 import rospy
 from hobbit_msgs.msg import Event
 from std_msgs.msg import Header
+LIMIT=10
 
 def talker():
     rospy.init_node('CRON_CF', anonymous=True)
@@ -9,8 +10,15 @@ def talker():
     data.header = Header()
     data.header.stamp = rospy.Time.now()
     data.event = 'E_CLEARFLOOR'
-    pub = rospy.Publisher('Event', Event, queue_size=10)
-    pub.publish(data)
+    pub = rospy.Publisher('/Event', Event, queue_size=10)
+    r = rospy.Rate(10)
+    i = 0
+    while i < LIMIT:
+        rospy.loginfo(str(data))
+        pub.publish(data)
+        r.sleep()
+        i += 1
+
 
 if __name__ == '__main__':
     try:
--- hobbit_smach/src/HelperTools/cron_GETUP.py
+++ hobbit_smach/src/HelperTools/cron_GETUP.py
@@ -2,6 +2,7 @@
 import rospy
 from hobbit_msgs.msg import Event
 from std_msgs.msg import Header
+LIMIT=10
 
 def talker():
     rospy.init_node('CRON_GETUP', anonymous=True)
@@ -10,7 +11,13 @@ def talker():
     data.header.stamp = rospy.Time.now()
     data.event = 'E_CALL'
     pub = rospy.Publisher('Event', Event, queue_size=10)
-    pub.publish(data)
+    r = rospy.Rate(10)
+    i = 0
+    while i < LIMIT:
+        rospy.loginfo(str(data))
+        pub.publish(data)
+        r.sleep()
+        i += 1
 
 if __name__ == '__main__':
     try:
