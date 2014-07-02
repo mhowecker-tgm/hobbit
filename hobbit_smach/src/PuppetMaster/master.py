ad221da10f499340a6039f46032e08dd47a49c7d
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-07-02 14:14:05 +0000

    master handles event,command data.
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7246 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_smach/src/PuppetMaster/master.py
+++ hobbit_smach/src/PuppetMaster/master.py
@@ -6,9 +6,10 @@ import roslib
 roslib.load_manifest(PKG)
 import rospy
 import smach_ros
-from smach import StateMachine, Concurrence, Sequence
-from hobbit_msgs.msg import Command, Status, Event
+from smach import StateMachine, Concurrence, State
+from hobbit_msgs.msg import Command, Event
 import uashh_smach.util as util
+from std_msgs.msg import String
 
 # sos = ['G_FALL', 'E_SOSBUTTON', 'C_HELP']
 # recharge = ['E_RECHARGE']
@@ -24,24 +25,36 @@ import uashh_smach.util as util
 # surprise = ['C_SURPRISE']
 # reward = ['C_REWARD']
 
-commands = [['G_FALL', 'E_SOSBUTTON', 'C_HELP', 'E_HELP'],
-            ['E_RECHARGE'],
-            ['C_STOP'],
-            ['E_CALLHOBBIT'],
-            ['C_BREAK'],
+actions = [['G_FALL', 'E_SOSBUTTON', 'C_HELP', 'E_HELP'],
+           ['E_RECHARGE'],
+           ['C_STOP'],
+           ['E_CALLHOBBIT'],
+           ['C_BREAK'],
+           ['E_REMINDER'],
+           ['E_CALLRING', 'E_CALLESTABLISHED', 'E_CALLENDED'],
+           ['C_MAKECALL'],
+           ['E_CLEARFLOOR'],
+           ['C_PICKUP', 'C_FOLLOW', 'C_LEARN', 'C_BRING', 'C_GOTO'],
+           ['E_PATROL'],
+           ['C_SURPRISE'],
+           ['C_REWARD']]
+commands = [['G_FALL', 'E_SOSBUTTON', 'C_HELP', 'E_HELP', 'C_HELP', 'F_CALLSOS', 'G_EMERGENCY'],
+            ['E_RECHARGE', 'C_RECHARGE'],
             ['E_REMINDER'],
-            ['E_CALLRING', 'E_CALLESTABLISHED', 'E_CALLENDED'],
-            ['C_MAKECALL'],
+            ['C_STOP', 'G_STOP'],
+            ['C_CALLHOBBIT'],
+            ['E_CALLRING', 'E_CALLESTABLISHED', 'E_CALLENDED', 'C_MAKECALL'],
             ['E_CLEARFLOOR'],
-            ['C_PICKUP', 'C_FOLLOW', 'C_LEARN', 'C_BRING', 'C_GOTO'],
+            ['C_PICKUP', 'C_FOLLOW', 'C_LEARN', 'C_BRING', 'C_GOTO', 'G_POINTING'],
             ['E_PATROL'],
             ['C_SURPRISE'],
-            ['C_REWARD']]
+            ['C_REWARD', 'G_REWARD']
+            ]
 
 
 def event_cb(msg, ud):
     rospy.loginfo('/Event data received:')
-    rospy.loginfo(str(msg))
+    print(msg.event)
     if rospy.has_param('active_task'):
         active_task = rospy.get_param('active_task')
     else:
@@ -50,19 +63,21 @@ def event_cb(msg, ud):
         if msg.event in item:
             if index + 1 >= active_task:
                 rospy.loginfo('New task has lower priority. Do nothing')
+                ud.command = msg.event
+                ud.params = msg.params
                 return False
             else:
                 rospy.loginfo('New task has higher priority. Start it.')
                 rospy.set_param('active_task', index)
                 return True
-        else:
-            rospy.loginfo('Unknown event received %s' % msg.event)
+    rospy.loginfo('Unknown event received %s' % msg.event)
     return False
 
 
 def command_cb(msg, ud):
     rospy.loginfo('/Command data received:')
     rospy.loginfo(str(msg))
+    print(msg)
     if rospy.has_param('active_task'):
         active_task = rospy.get_param('active_task')
     else:
@@ -71,14 +86,59 @@ def command_cb(msg, ud):
         if msg.command in item:
             if index + 1 >= active_task:
                 rospy.loginfo('New task has lower priority. Do nothing')
+                ud.command = msg.event
+                ud.params = msg.params
                 return False
             else:
                 rospy.loginfo('New task has higher priority. Start it.')
                 rospy.set_param('active_task', index)
                 return True
-        else:
-            rospy.loginfo('Unknown command received %s' % msg.command)
+    rospy.loginfo('Unknown command received %s' % msg.command)
     return False
+
+
+def child_cb(outcome_map):
+    print('child_cb')
+    return True
+
+
+def child_cb1(outcome_map):
+    print('child_cb1')
+    return False
+
+
+
+class Init(State):
+    """Initialize a few data structures
+    """
+    def __init__(self):
+        State.__init__(
+            self,
+            outcomes=['succeeded', 'preempted'])
+
+    def execute(self, ud):
+        if self.preempt_requested():
+            self.service_preempt()
+            return 'preempted'
+        print('Init')
+        return 'succeeded'
+
+class TestASW(State):
+    """class to test the ASW functionality
+    """
+    def __init__(self):
+        State.__init__(
+            self,
+            input_keys=['command'],
+            outcomes=['succeeded', 'preempted', 'aborted'])
+
+    def execute(self, ud):
+        if self.preempt_requested():
+            self.service_preempt()
+            return 'preempted'
+        print('TestASW')
+        print(ud.command)
+        return 'succeeded'
 
 
 def main():
@@ -86,14 +146,38 @@ def main():
     sm = StateMachine(
         outcomes=['succeeded',
                   'preempted',
-                  'failed']
+                  'failed'],
+        input_keys=['command'],
+        output_keys=['command'],
     )
+
+    sm1 = StateMachine(
+        outcomes=['succeeded',
+                  'preempted',
+                  'failed'],
+        input_keys=['command'],
+        output_keys=['command'],
+    )
+    sm1.userdata.command = 'IDLE'
 
     cc = Concurrence(
         outcomes=['succeeded', 'aborted', 'preempted'],
         default_outcome='aborted',
+        input_keys=['command'],
+        output_keys=['command'],
+        child_termination_cb=child_cb,
         outcome_map={'succeeded': {'Event_Listener': 'succeeded'},
                      'aborted': {'Event_Listener': 'aborted'}}
+    )
+
+    cc1 = Concurrence(
+        outcomes=['succeeded', 'aborted', 'preempted'],
+        default_outcome='aborted',
+        input_keys=['command'],
+        output_keys=['command'],
+        child_termination_cb=child_cb1,
+        outcome_map={'succeeded': {'ASW': 'succeeded'},
+                     'aborted': {'Commands': 'failed'}}
     )
 
     with cc:
@@ -102,7 +186,8 @@ def main():
             util.WaitForMsgState(
                 '/Event',
                 Event,
-                msg_cb=event_cb
+                msg_cb=event_cb,
+                output_keys=['command', 'params']
             )
         )
         Concurrence.add(
@@ -110,38 +195,50 @@ def main():
             util.WaitForMsgState(
                 '/Command',
                 Command,
-                msg_cb=command_cb
+                msg_cb=command_cb,
+                output_keys=['command', 'params']
             )
         )
-        # Concurrence.add(
-        #     'Status_Listener',
-        #     util.WaitForMsgState(
-        #         '/Status',
-        #         Status,
-        #         msg_cb=status_cb
-        #     )
-        # )
 
     with sm:
-        # StateMachine.add(
-        #     'START',
-        #     Start(),
-        #     transitions={'succeeded': 'WAIT_FOR_E_C',
-        #                  'aborted': 'SET_FAILURE'}
-        # )
         StateMachine.add(
             'WAIT_FOR_E_C',
             cc,
-            transitions={'succeeded': 'WAIT_FOR_E_C',
+            transitions={'succeeded': 'succeeded',
                          'aborted': 'WAIT_FOR_E_C',
                          'preempted': 'preempted'}
         )
 
-    sis = smach_ros.IntrospectionServer('master', sm, '/MASTER')
+    with cc1:
+        Concurrence.add(
+            'ASW',
+            TestASW()
+        )
+        Concurrence.add(
+            'Commands',
+            sm
+        )
+
+    with sm1:
+        StateMachine.add(
+            'INIT',
+            Init(),
+            transitions={'succeeded': 'MAIN',
+                         'preempted': 'preempted'}
+        )
+        StateMachine.add(
+            'MAIN',
+            cc1,
+            transitions={'succeeded': 'MAIN',
+                         'aborted': 'MAIN',
+                         'preempted': 'preempted'}
+        )
+
+    sis = smach_ros.IntrospectionServer('master', sm1, '/MASTER')
     sis.start()
 
-    outcome = sm.execute()
-    rospy.loginfo(NAME + ' returned outcome' + str(outcome))
+    outcome = sm1.execute()
+    rospy.loginfo(NAME + ' returned outcome ' + str(outcome))
     rospy.spin()
     sis.stop()
 
