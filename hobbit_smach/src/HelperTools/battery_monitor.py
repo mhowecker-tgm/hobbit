f0fad1827beba40f8ee0ffe41821bb37030735a2
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-07-08 07:01:38 +0000

    helper node that monitors the battery levels
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7306 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

---
+++ hobbit_smach/src/HelperTools/battery_monitor.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import rospy
+from hobbit_msgs.msg import Event
+from std_msgs.msg import Header
+from mira_msgs.msg import BatteryState
+
+LIMIT = 5
+NAME = 'BATTERY_CHECK'
+UPPER_LIMIT = 95 # High values to test the master node
+LOWER_LIMIT = 90
+
+
+def talker(self, level):
+    data = Event()
+    data.header = Header()
+    data.header.stamp = rospy.Time.now()
+    data.event = level
+    pub = rospy.Publisher('/Event', Event, queue_size=10)
+    r = rospy.Rate(10)
+    i = 0
+    while i < LIMIT:
+        rospy.loginfo(str(data))
+        pub.publish(data)
+        r.sleep()
+        i += 1
+
+
+def battery_cb(self, msg):
+    print('received msg on topic /battery_state')
+    print(msg)
+    if msg.lifePercent < UPPER_LIMIT:
+        rospy.loginfo('Battery level is low')
+        talker('E_RECHARGE')
+    if msg.lifePercent < LOWER_LIMIT:
+        rospy.loginfo('Battery level is low')
+        talker('E_RECHARGE_LOW')
+    else:
+        pass
+
+
+def main():
+    rospy.init_node(NAME, anonymous=False)
+    rospy.Subscriber('battery_state', BatteryState, battery_cb)
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException: pass
