6e95c1d94c0a4a5185e7b9ab40a4566739075fe8
david.fischinger <david.fischinger@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-08-04 16:14:23 +0000

    integrated source code for point_cloud_edit and pc_to_iv packages into table_object_detector package
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7587 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

---
+++ table_object_detector/src/iv_to_pc.cpp
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <stdlib.h>     /* atof */
+
+//PCL includes
+#include "pcl/point_cloud.h"
+#include "pcl/point_types.h"
+#include "pcl/ros/conversions.h"
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/vtk_io.h>
+#include "/opt/ros/hobbit_hydro/src/table_object_detector/include/iv_io.h"
+
+//#include <opencv2/highgui.h>
+
+//Ros includes
+#include <ros/ros.h>
+#include "pcl_ros/publisher.h"
+#include "sensor_msgs/PointCloud2.h"
+#include "std_msgs/String.h"
+//new 30.1.2014
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+//#include <tuw_kuka_manipulation_msgs/RecogResult.h>
+
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+
+#define BUFSIZE 512
+
+//using namespace pcl;
+//using namespace pcl::io;
+using namespace std;
+
+ros::Publisher new_pc_pub;
+ros::Publisher pub_path_to_ivfilename;
+
+/*
+void publishPathToNewIVFile(tuw_kuka_manipulation_msgs::RecogResult recogresult){
+	//this function is triggered if object was recognized. it published path to iv-filename on topic (this triggers the default funktion of this program)
+	ROS_INFO("--> publishPathToNewIVFile(): Publishing path to iv-file from recognition (triggers publishing of iv-file as point cloud)");
+	ROS_INFO("recogresult->xml_iv_model: ");
+	cout << recogresult.xml_iv_model << endl;
+	size_t start, end;
+	string tmp=recogresult.xml_iv_model;
+	string path_iv_file;
+	string strPattern ("=");	//if this pattern is found in iv-file, it should be a line with point data
+	string strPattern2 (">");
+	start = tmp.find(strPattern);
+	end = tmp.find(strPattern2);
+	path_iv_file = tmp.substr(start+2, end-start-3);
+	cout << "path_iv_file: " << path_iv_file << endl;
+
+	// convert message to string for ROS node communication
+	std_msgs::String msg;
+	std::stringstream ss;
+	ss << path_iv_file;
+	//ROS_INFO("%s", ss.str().c_str());
+	msg.data = ss.str();
+	pub_path_to_ivfilename.publish(msg);
+
+}
+*/
+
+
+void generateAndPublishIVFile(const std_msgs::String pathtoivfile){
+
+	ROS_INFO("--> generateAndPublshIVFile(): Generating new point cloud from iv-file");
+	string path_to_iv_file = pathtoivfile.data ;
+	cout << "Generate point cloud from file: " << path_to_iv_file << endl;
+
+	//point cloud initialization
+	sensor_msgs::PointCloud2 pc2;
+	PointCloud::Ptr msg (new PointCloud);
+
+
+
+	ifstream ivfile;	//txt-file with iv-data (points)
+	ivfile.open(path_to_iv_file.c_str());
+	if (!ivfile){
+		cout << "\n PROBLEM opening file: " << path_to_iv_file << endl;
+	}
+
+	string line;
+	getline(ivfile, line);
+	//can be used if files have more than one line
+	int lines_to_skip = 8;
+	for (unsigned int line_nr = 1; line_nr <= lines_to_skip; ++line_nr){
+		getline(ivfile, line);
+	}
+	string strPattern ("0.");	//if this pattern is found in iv-file, it should be a line with point data
+	string strPattern2 (",");
+	size_t found, found2, found3;
+	string p1,p2;
+	string p_x_str, p_y_str, p_z_str;
+	double p_x, p_y, p_z;
+	while (ivfile.good())
+	{
+		getline(ivfile, line);
+		found = line.find(strPattern);
+		if (found!=string::npos){	//<==> if strPattern was detected in line <==> line includes point data
+			//print => fill pc
+			found2 = line.find(strPattern2);
+			found3 = line.rfind(strPattern2);
+			p1 = line.substr(found, found2-found);
+			p2 = line.substr(found2+2,found3-found2);
+			p_x_str = p1.substr(0,p1.find(" "));
+			p_y_str = p1.substr(p1.find(" ")+1,p1.rfind(" ")-p1.find(" "));
+			p_z_str = p1.substr(p1.rfind(" ")+1);
+			p_x = atof(p_x_str.c_str());
+			p_y = atof(p_y_str.c_str());
+			p_z = atof(p_z_str.c_str());
+			//cout << p1 << ' \n david' << p2 << '\n';
+			//cout << p_x_str << " " << p_y_str << " " << p_z_str << endl;
+			//msg->points.push_back (pcl::PointXYZ(p_x, p_y, p_z));
+			pcl::PointXYZ pnt_tmp = pcl::PointXYZ(p_x, p_y, p_z);
+			msg->points.push_back (pnt_tmp);
+		} else {
+			break;
+		}
+	}
+
+	ivfile.close();
+
+	pc2.header.frame_id = "lwr_";
+	pc2.header.stamp = ros::Time::now ();
+	msg->header = pcl_conversions::toPCL(pc2.header);
+
+	new_pc_pub.publish(msg);
+
+
+	return;
+}
+
+
+
+int main(int argc, char **argv){
+
+  //Subscribe to topic were path to iv file is published
+  ros::init (argc, argv, "iv_to_pc");
+  ros::NodeHandle nh;
+  new_pc_pub = nh.advertise<PointCloud> ("/SS/points2_object_in_rcs", 1);
+  //iv_filename_pub = nh.advertise<std_msgs::String>("/pc_to_iv/generated_ivfilename",10);
+  //ros::Duration(0.3).sleep(); //needed?
+  pub_path_to_ivfilename = nh.advertise<std_msgs::String> ("/path_to_ivfilename", 1);
+  ros::Subscriber sub = nh.subscribe("/path_to_ivfilename", 1, generateAndPublishIVFile);
+  //ros::Subscriber sub_add_object_from_recognition = nh.subscribe("/segmentation_recognition/add_object", 1, publishPathToNewIVFile); //Aitor recognition
+  //ros::Subscriber sub = nh.subscribe("/SS/points2_object_in_rcs", 10, generateInventor);
+  ROS_INFO("Starting node: iv_to_pc");
+  ros::spin();
+  return 0;
+}
---
+++ table_object_detector/src/pc_merge.cpp
@@ -0,0 +1,173 @@
+/*
+ *
+ *
+ * David Fischinger -TUW
+ * 22.1.2014
+ * earlier version: 26.03. 2012
+ *
+ * input:
+ *
+ *   pointcloud from topcamera read from topic /SS/camera/depth_registered/points
+ *
+ * output:
+ *   pointcloud edited
+ *
+ *   output point cloud w.r.t. tf_frame /lwr on topic /SS/points2_object_in_rcs
+ *
+ *
+ */
+
+#include <pcl_ros/point_cloud.h>
+#include <pc_merge_with_basket_rot.hpp>
+#include <ros/ros.h>
+#include <pcl/point_types.h>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_listener.h>
+#include "pcl_ros/transforms.h"
+
+#include "tf/tf.h"
+#include "tf/transform_broadcaster.h"
+#include "tf/transform_datatypes.h"
+#include "tf/transform_listener.h"
+
+
+CPCMerge::CPCMerge(ros::NodeHandle nh_)
+{
+  nh = nh_;
+  pc_cam1_filled = false;
+  //define publishers
+  pc_for_basketdet_pub = nh.advertise<pcl::PointCloud<pcl::PointXYZ> >("/SS/points2_object_in_rcs",1); //rcs:robot coordinate sys.
+  position_highestpoint_pub = nh.advertise<std_msgs::String>("/SS/basket_position",1);
+  //define subscriber
+  pc_cam1_sub = nh.subscribe("/SS/camera/depth_registered/points",1, &CPCMerge::pc_cam1_callback, this);
+}
+
+void CPCMerge::pc_cam1_callback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& pcl_in)
+{
+  m.lock();
+
+  ROS_INFO("pc_cam1 received");
+
+  this->pc_cam1_filled = false;
+
+  //search for tf transform for pc from cam1
+  ros::Time now = ros::Time::now();
+  bool foundTransform = tf_listener.waitForTransform("/lwr", "/camera_depth_optical_frame", now
+		                                             /*(*pcl_in).header.stamp*/, ros::Duration(13.0));
+  if (!foundTransform)
+  {
+	ROS_WARN("No pc_cam1 transform found");
+	m.unlock();
+	return;
+  }
+
+  //ROS_INFO(tf_listener);
+  ROS_INFO("Transform pc_cam1: camera_depth_optical_frame to lwr found");
+  pcl_ros::transformPointCloud("/lwr", *pcl_in, pc_cam1, tf_listener);
+  this->pc_cam1_filled = true;
+
+  //publishes point cloud
+  publish_merged_pc();
+  
+  m.unlock();
+}
+
+
+void CPCMerge::publish_merged_pc()
+{
+	ROS_INFO("publish_merged_pc() started");
+	pcl::PointCloud<pcl::PointXYZ> pcl_cloud_merged;
+	pcl_cloud_merged = pc_cam1;
+
+	//Filter Data (with basket detection and basket point elimination)
+	filter_pc(pcl_cloud_merged, false); //last entry false <=> coordinates of highest points of scene are published instead of basket center point
+
+    //publish manipulated and merged point cloud data (in lwr (not world!) coordinate system)
+    pc_for_basketdet_out = pcl_cloud_merged;
+    pc_for_basketdet_out.header.frame_id = "/lwr";
+	pc_for_basketdet_pub.publish(pc_for_basketdet_out);
+
+	ROS_INFO("point cloud of objects (rcs) and highest point position (of objects) were published");
+
+        this->pc_cam1_filled = false;
+}
+
+// filter point cloud and cut off points outside a defined region
+//if pub_basket_position == false than the position of the highest point is returned! otherwise the center of basket
+void CPCMerge::filter_pc(pcl::PointCloud<pcl::PointXYZ>& pcl_cloud_merged, bool pub_basket_position, float x_min, float x_max, float y_min, float y_max, float z_min, float z_max )
+{
+	// Create the filtering object
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered_z (new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered_y (new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered_x (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+    *cloud = pcl_cloud_merged;
+	pcl::PassThrough<pcl::PointXYZ> pass;
+
+	ROS_INFO("Filtering outliers and cutting region");
+	ROS_INFO("points before filtering: %d", pcl_cloud_merged.points.size());
+	//Filter w.r.t. axis z
+	pass.setInputCloud (cloud);
+	pass.setFilterFieldName ("z");
+	pass.setFilterLimits (z_min, z_max);
+	//pass.setFilterLimitsNegative (true);
+	pass.filter (*cloud_filtered_z);
+	ROS_INFO("points after z filtering: %d", cloud_filtered_z->points.size());
+	//Filter w.r.t. axis y
+	pass.setInputCloud(cloud_filtered_z);
+	pass.setFilterFieldName ("y");
+	pass.setFilterLimits (y_min, y_max);
+	pass.filter (*cloud_filtered_y);
+	ROS_INFO("points after y filtering: %d", cloud_filtered_y->points.size());
+	//Filter w.r.t. axis x
+	pass.setInputCloud(cloud_filtered_y);
+	pass.setFilterFieldName ("x");
+	pass.setFilterLimits (x_min, x_max);
+	pass.filter (*cloud_filtered_x);
+	ROS_INFO("points after x filtering: %d", cloud_filtered_x->points.size());
+	//Create the filtering object
+	pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
+	sor.setInputCloud(cloud_filtered_x);
+	sor.setMeanK(50);
+	sor.setStddevMulThresh (1.0);
+	sor.filter(pcl_cloud_merged);
+
+	if (pub_basket_position == false){ // than the position of the highest point is published!
+	    ROS_INFO("%d",pcl_cloud_merged.points.size());
+		float z_max = -1000;			//max heigt val for z
+		float x_for_z_max = -1000;		//coordinates for x,y at position of maximal z
+		float y_for_z_max = -1000;
+	    for (unsigned int i = 0; i < pcl_cloud_merged.points.size(); ++i)
+		{
+		  if (pcl_cloud_merged.points[i].z > z_max)
+		  {
+			z_max = pcl_cloud_merged.points[i].z;
+			x_for_z_max = pcl_cloud_merged.points[i].x;
+			y_for_z_max = pcl_cloud_merged.points[i].y;
+		  }
+		}
+	    //Publish coordinates of heighest points ("basket center" if there is no basket)
+		std_msgs::String msgStrCorners;
+		std::stringstream ss;
+		ss << x_for_z_max << " "  << y_for_z_max << " " << 0;
+		msgStrCorners.data = ss.str();
+		position_highestpoint_pub.publish(msgStrCorners);
+	} else {
+		basket_detection<pcl::PointXYZ>(pcl_cloud_merged,pub_basket_position);
+	}
+}
+
+
+
+int main (int argc, char** argv)
+{
+  ROS_INFO("ROS NODE pc_merge_1cam_nobox started (point_cloud_edit)");
+  ros::init(argc, argv, "point_cloud_edit");
+  ros::NodeHandle nh;
+  CPCMerge * pc_merge = new CPCMerge(nh);
+
+  ros::spin();
+  return (0);
+}
