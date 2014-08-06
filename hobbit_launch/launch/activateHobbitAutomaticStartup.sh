725739159a1c69518a8c69201529ae743c803030
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-08-06 11:06:45 +0000

    ...
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7605 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_launch/launch/activateHobbitAutomaticStartup.sh
+++ hobbit_launch/launch/activateHobbitAutomaticStartup.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+
+red=$(printf "\033[31m")
+normal=$(printf "\033[m")
+
 echo "Will now activate automatic hobbit startup script execution on each boot"
+
+if [ ! -e  /opt/ros/hobbit_hydro/src/hobbit_launch/launch/hobbitPT2.desktop ]
+then
+ echo $red
+  echo "Could not find hobbit shortcut file , cannot enable automatic startup"
+ echo $normal
+ exit 0
+fi
+
+
 cd ~/.config
 mkdir autostart
 cd autostart
--- hobbit_launch/launch/initializeHobbitInstallation.sh
+++ hobbit_launch/launch/initializeHobbitInstallation.sh
@@ -3,16 +3,57 @@
 red=$(printf "\033[31m")
 normal=$(printf "\033[m")
 
-
+#Check Super User
 if [ $( id -u ) -eq 0 ]; then
-echo "Will setup things that need super user powers now"
-else
- echo $red
- echo "Initializer must be run as root in order to have system permissions.."
- echo "Please re run using sudo ./initializeHobbit.sh , exiting now.."
+ echo $red 
+ echo "Please run this script as a regular user , it will ask you for super user powers when needed "
  echo $normal
  exit 0
+else
+ echo "Working.."
 fi
+
+#Change to Script directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
+#Link hobbit to startup
+./activateHobbitAutomaticStartup.sh
+
+#Generate Scripts
+OURUSER=`whoami`
+
+mkdir System
+sh -c 'echo "#!/bin/bash \n sudo init 0\n exit 0\n" > System/shutdown.sh' 
+sudo chown root:root System/shutdown.sh
+sudo chmod 777 System/shutdown.sh
+sudo chmod +s System/shutdown.sh
+
+sh -c 'echo "#!/bin/bash \n sudo init 6\n exit 0\n" > System/restart.sh' 
+sudo chown root:root System/restart.sh
+sudo chmod 777 System/restart.sh
+sudo chmod +s System/restart.sh
+
+sh -c 'echo "#!/bin/bash \n \
+             DIR=\"\$( cd \"\$( dirname \"\${BASH_SOURCE[0]}\" )\" && pwd )\" \n  \
+             cd \$DIR  \n \
+             cd ..        \n \
+             cd ..        \n \
+             cd ..        \n \
+             svn update . \n exit 0 \n" > System/update.sh' 
+
+sudo chown "$OURUSER":"$OURUSER" System/update.sh
+#sudo chmod 777 System/update.sh
+#sudo chmod +s System/update.sh
+
+
+
+sh -c 'echo "#!/bin/bash \n \
+             sudo apt-get update && sudo apt-get upgrade -y \n exit 0 \n" > System/updateGlobal.sh'
+
+sudo chown root:root System/updateGlobal.sh
+sudo chmod 777 System/updateGlobal.sh
+sudo chmod +s System/updateGlobal.sh
 
 
 exit 0
