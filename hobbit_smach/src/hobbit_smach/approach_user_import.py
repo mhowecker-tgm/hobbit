4f6c91a3d487854dd282ca4274d82075e6ccfbe8
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-08-06 09:43:20 +0000

    add another version of approach user.
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7603 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_smach/src/hobbit_smach/approach_user_import.py
+++ hobbit_smach/src/hobbit_smach/approach_user_import.py
@@ -2,148 +2,161 @@
 # -*- coding: utf-8 -*-
 
 PKG = 'hobbit_smach'
-NAME = 'hobbit_move'
-DEBUG = True
+NAME = 'SocialRole'
 
 import roslib
 roslib.load_manifest(PKG)
-import rospy
 
-from smach import State, Sequence, StateMachine
-#import uashh_smach.util as util
-import hobbit_smach.hobbit_move_import as hobbit_move
+from smach import Sequence, State, StateMachine
 import hobbit_smach.speech_output_import as speech_output
+import hobbit_smach.hobbit_move_import as hobbit_move
 
 
-class CalcPoseAtDistance(State):
+
+def approach_seq1():
     """
-    Read the user's current pose from userdata and calculates the pose that
-    the robot should move to before trying to get the user's attention.
-
-    """
-    def __init__(self, angle=0):
-        State.__init__(
-            self,
-            input_keys=['user_pose', 'x', 'y', 'yaw'],
-            output_keys=['x', 'y', 'yaw'],
-            outcomes=['succeeded', 'preempted', 'aborted']
-        )
-
-    def execute(self, ud):
-        if self.preempt_requested():
-            self.service_preempt()
-            return 'preempted'
-        if DEBUG:
-            print(ud.user_pose)
-            print(ud.x, ud.y, ud.yaw)
-        # TODO: clever calculation so that we are always close to the user
-        return 'succeeded'
-
-
-class CalcPoseNearUser(State):
-    """
-    Read the user's current pose from userdata and calculates the pose that
-    the robot should move to for touchscreen-based interaction.
-
-    """
-    def __init__(self, angle=0):
-        State.__init__(
-            self,
-            input_keys=['user_pose', 'x', 'y', 'yaw'],
-            output_keys=['x', 'y', 'yaw'],
-            outcomes=['succeeded', 'preempted', 'aborted']
-        )
-
-    def execute(self, ud):
-        if self.preempt_requested():
-            self.service_preempt()
-            return 'preempted'
-        if DEBUG:
-            print(ud.user_pose)
-            print(ud.x, ud.y, ud.yaw)
-        # TODO: clever calculation so that we are always close to the user
-        return 'succeeded'
-
-
-class CheckSocialRole(State):
-    """
-    """
-    def __init__(self, angle=0):
-        State.__init__(
-            self,
-            outcomes=['0', '1', '2', '3', '4', 'preempted', 'aborted']
-        )
-        self._angle = angle
-
-    def execute(self, ud):
-        if self.preempt_requested():
-            self.service_preempt()
-            return 'preempted'
-        social_role = rospy.get_param('social_role')
-        if DEBUG:
-            print(social_role)
-        if 0 <= int(social_role) < 5:
-            return social_role
-        else:
-            return 'aborted'
-
-
-def approachUser():
-    """
-    Return a SMACH state machine that will move the robot to the user and
-    depending on the social_role stop at a given distance to make some noise
-    to get the user's attention.
+    Returns a SMACH Sequence that will take the given user pose,
+    transforms it to world coordinates and based on the social role
+    adapts the approach to the user.
     """
 
-    seq1 = Sequence(
-        outcomes=['succeeded', 'preempted', 'aborted'],
+    seq = Sequence(
+        outcomes=['succeeded', 'preempted', 'failed'],
         connector_outcome='succeeded'
     )
-    seq2 = Sequence(
-        outcomes=['succeeded', 'preempted', 'aborted'],
+
+    with seq:
+        Sequence.add(
+            'CALC_END_POSE',
+            CalculateEndPose()
+        )
+        Sequence.add(
+            'MOVE_TO_END_POSE',
+            hobbit_move.goToPose()
+        )
+    return seq
+
+
+def approach_seq2():
+    """
+    Returns a SMACH Sequence that will take the given user pose,
+    transforms it to world coordinates and based on the social role
+    calculates an intermediate pose.
+    """
+
+    seq = Sequence(
+        outcomes=['succeeded', 'preempted', 'failed'],
         connector_outcome='succeeded'
     )
+
+    with seq:
+        Sequence.add(
+            'CALC_INT_POSE',
+            CalculateEndPose()
+        )
+        Sequence.add(
+            'MOVE_TO_INT_POSE',
+            hobbit_move.goToPose()
+        )
+        # Sequence.add(
+        #     'MAKE_SOME_NOISE',
+        #     # TODO: Write functionality for this.
+        # )
+    return seq
+
+
+def approach_seq():
+    """
+    Returns a SMACH StateMachine that does the approach the user scenario.
+    """
     sm = StateMachine(
-        outcomes=['succeeded', 'preempted', 'aborted']
+        outcomes=['succeeded', 'aborted', 'preempted']
     )
-
-    with seq1:
-        Sequence.add('GET_ROBOT_POSE', hobbit_move.getRobotPose())
-        Sequence.add('CALC_POSE_NEAR_USER', CalcPoseNearUser())
-        Sequence.add('MOVE_TO_POSE', hobbit_move.goToPose)
-        # TODO: What is the correct text ID to get attention from the user
-        Sequence.add('GET_ATTENTION', speech_output.sayText(info='Hey there'))
-
-    with seq2:
-        Sequence.add('GET_ROBOT_POSE', hobbit_move.getRobotPose())
-        Sequence.add('CALC_POSE_AT_DISTANCE', CalcPoseAtDistance())
-        Sequence.add('MOVE_TO_POSE', hobbit_move.goToPose)
-
     with sm:
         StateMachine.add(
-            'CHECK_SOCIAL_ROLE',
-            CheckSocialRole(),
-            transitions={'preempted': 'preempted',
-                         'aborted': 'aborted',
-                         '0': 'APPROACH_WITH_PAUSE',
-                         '1': 'APPROACH_WITH_PAUSE',
-                         '2': 'APPROACH_WITH_PAUSE',
-                         '3': 'APPROACH_DIRECT',
-                         '4': 'APPROACH_DIRECT'}
+            'SOCIAL_ROLE_CHECK',
+            socialRoleCheck(),
+            transitions={'tool': 'GOTO_END_POSE',
+                         'butler': 'GOTO_END_POSE',
+                         'companion': 'GOTO_INT_POSE'}
         )
-
         StateMachine.add(
-            'APPROACH_WITH_PAUSE',
-            seq1,
-            transitions={'succeeded': 'APPROACH_DIRECT',
+            'GOTO_INT_POSE',
+            approach_seq2,
+            transitions={'succeeded': 'GOTO_END_POSE',
                          'aborted': 'aborted',
                          'preempted': 'preempted'}
         )
-
         StateMachine.add(
-            'APPROACH_DIRECT',
-            seq2,
+            'GOTO_END_POSE',
+            approach_seq2,
             transitions={'succeeded': 'succeeded',
                          'aborted': 'aborted',
                          'preempted': 'preempted'}
         )
+
+class CalculateNextPose(State):
+    """
+    Given the user pose in world coordinates calculate the pose at
+    which the robot should stop during its approach to the user.
+    """
+
+    def __init__(self):
+        State.__init__(
+            self,
+            outcomes=['succeeded', 'aborted', 'preempted'],
+            output_keys=['x', 'y', 'yaw']
+        )
+
+    def execute(self, ud):
+        if self.preempt_requested():
+            self.service_preempt()
+            return 'preempted'
+        return 'succeeded'
+
+
+class CalculateEndPose(State):
+    """
+    Given the user pose in world coordinates calculate the pose at
+    which the robot should be at the end of its approach to the user.
+    """
+
+    def __init__(self):
+        State.__init__(
+            self,
+            outcomes=['succeeded', 'aborted', 'preempted'],
+            output_keys=['x', 'y', 'yaw']
+        )
+
+    def execute(self, ud):
+        if self.preempt_requested():
+            self.service_preempt()
+            return 'preempted'
+        return 'succeeded'
+
+
+class socialRoleCheck(State):
+    """
+    This class checks the social_role of the robot and returns the correct
+    outcome that corresponds with it.
+    """
+
+    def __init__(self):
+        State.__init__(
+            self,
+            outcomes=['butler', 'companion', 'tool', 'preempted'],
+            input_keys=['social_role']
+        )
+
+    def execute(self, ud):
+        if self.preempt_requested():
+            self.service_preempt()
+            return 'preempted'
+        if ud.social_role == 0:
+            return 'tool'
+        elif 1 <= ud.social_role <= 3:
+            return 'butler'
+        else:
+            return 'companion'
+
+
