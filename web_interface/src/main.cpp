eabf2fc6c8f0086b82064b82e224ed447514e60e
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-08-07 13:24:03 +0000

    ...
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7663 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- web_interface/src/main.cpp
+++ web_interface/src/main.cpp
@@ -549,9 +549,9 @@ void execute(char * command,char * param)
   if (strcmp(command,"camera")==0)
   {
     if (strcmp(param,"refreshTop")==0)
-        { strncpy(cR,"/bin/bash -c \"cd /opt/ros/hobbit_hydro/src/rgbd_acquisition/bin/frames/top/ && timeout 1 rosrun image_view image_saver image:=/headcam/rgb/image_raw\" ",cRLen); }
+        { strncpy(cR,"/bin/bash -c \"cd ../../rgbd_acquisition/bin/frames/top/ && timeout 1 rosrun image_view image_saver image:=/headcam/rgb/image_raw\" ",cRLen); }
     if (strcmp(param,"refreshBottom")==0)
-        { strncpy(cR,"/bin/bash -c \"cd /opt/ros/hobbit_hydro/src/rgbd_acquisition/bin/frames/base/ && timeout 1 rosrun image_view image_saver image:=/basecam/rgb/image_raw\" ",cRLen); }
+        { strncpy(cR,"/bin/bash -c \"cd ../../rgbd_acquisition/bin/frames/base/ && timeout 1 rosrun image_view image_saver image:=/basecam/rgb/image_raw\" ",cRLen); }
     if (strcmp(param,"refreshTF")==0)
       { strncpy(cR,"/bin/bash -c \"cd ../../rgbd_acquisition/bin/frames/tf/ && rosrun tf view_frames && convert frames.pdf frames.png\" ",cRLen); }
 
