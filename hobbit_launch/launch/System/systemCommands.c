8d5cb4a2ae16a8f48337a53e7fd0c7149991721c
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-08-08 12:33:58 +0000

    ...
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7680 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_launch/launch/System/systemCommands.c
+++ hobbit_launch/launch/System/systemCommands.c
@@ -11,8 +11,8 @@ int main(int argc, char *argv[])
    for (i=0; i<argc; i++)
    {
      if (strcmp(argv[i],"updateGlobal")==0) { system("apt-get update && apt-get upgrade -y"); } else
-     if (strcmp(argv[i],"shutdown")==0) { system("shutdown -H"); } else
-     if (strcmp(argv[i],"restart")==0) { system("shutdown -r"); } 
+     if (strcmp(argv[i],"shutdown")==0) { system("shutdown -H -t 00"); } else
+     if (strcmp(argv[i],"restart")==0) { system("shutdown -r -t 00"); } 
    }
 
    return 0;
