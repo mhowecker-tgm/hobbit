0091e24f5e946f042181d8634331044f7806be43
paloma.puente <paloma.puente@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-08-12 08:17:39 +0000

    new mira visualization command in startup script
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7696 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_launch/launch/startupHobbitPT2.sh
+++ hobbit_launch/launch/startupHobbitPT2.sh
@@ -102,10 +102,12 @@ roslaunch startup.launch&
 
 sleep $DELAY_BETWEEN_STEPS
 
-# Start mira (start mira center with: miracenter -k ipaddress:1234', e.g. 'miracenter -k 127.0.0.1:1234')
+# Start mira (start mira center with: e.g. 'miracenter mira_vis_config.xml')
 cd /opt/ros/hobbit_hydro/src/interfaces_mira/resources
-#mira mira_config.xml -p1234&
-miracenter mira_config.xml
+mira mira_config.xml -p1234&
+#miracenter mira_config.xml
+# start new mira visualization
+miracenter mira_vis_config.xml
 
 #Startup Joystick node , ( it allows webinterface joystick emulation also )
 roslaunch joy2twist startup.launch& 
