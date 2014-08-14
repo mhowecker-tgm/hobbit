d18f492a1e3f7885333b80df0712010c11769a1a
david.fischinger <david.fischinger@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-08-14 13:49:00 +0000

    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7734 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_launch/launch/startupHobbitPT2.sh
+++ hobbit_launch/launch/startupHobbitPT2.sh
@@ -103,17 +103,16 @@ roslaunch startup.launch&
 sleep $DELAY_BETWEEN_STEPS
 
 # Start mira (start mira center with: e.g. 'miracenter mira_vis_config.xml')
-cd /opt/ros/hobbit_hydro/src/interfaces_mira/resources
+#cd /opt/ros/hobbit_hydro/src/interfaces_mira/resources
 mira mira_config.xml -p1234&
-#miracenter mira_config.xml
 sleep $DELAY_BETWEEN_STEPS
 # start new mira visualization
-miracenter mira_vis_config.xml
+miracenter mira_vis_config.xml&
 
 #start get_current_room
-sleep $DELAY_BETWEEN_STEPS
-cd /opt/ros/hobbit_hydro/src/get_current_room/launch
-roslaunch startup.launch&
+#sleep $DELAY_BETWEEN_STEPS
+#cd /opt/ros/hobbit_hydro/src/get_current_room/launch
+#roslaunch startup.launch&
 
 #Startup Joystick node , ( it allows webinterface joystick emulation also )
 roslaunch joy2twist startup.launch& 
