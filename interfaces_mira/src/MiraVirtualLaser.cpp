e252c34c41f0cbadde300f5d5c1f5a581f88c058
paloma.puente <paloma.puente@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-08-19 16:17:44 +0000

    mira virtual laser obs max range changed
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7749 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- interfaces_mira/src/MiraVirtualLaser.cpp
+++ interfaces_mira/src/MiraVirtualLaser.cpp
@@ -79,8 +79,8 @@ void MiraVirtualLaser::virtual_laser_obs_callback(const sensor_msgs::LaserScan::
 
 	scan.minimumRange = msg->range_min;
 	//scan.maximumRange = msg->range_max; //ok for localization
-	//scan.maximumRange = 0.99; //FIXME //value for temporary obstacle avoidance
-	scan.maximumRange = 3;
+	scan.maximumRange = 0.99; //FIXME //value for temporary obstacle avoidance
+	//scan.maximumRange = 3;
 
 	for (int i=0; i<msg->ranges.size();i++)
 	{
