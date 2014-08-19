e7d499558c94ab8969604e93cd80abef16cc3174
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-08-19 11:13:37 +0000

    ...
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7745 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

---
+++ hobbit_launch/launch/switchMira.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters, valid modes are learning,mapping,navigation"
+fi
+
+if [ "$1" == "learning" ]
+then
+ echo "Learning"
+  killall -9 mira
+
+  cd /opt/ros/hobbit_hydro/src/interfaces_mira/launch
+  roslaunch startup.launch&
+
+  cd /opt/ros/hobbit_hydro/src/interfaces_mira/resources
+  miracenter mira_config.xml&
+
+  cd /opt/ros/hobbit_hydro/src/get_current_room/launch
+  roslaunch startup.launch&
+
+  cd /opt/ros/hobbit_hydro/src/places_learning_gui/launch
+  roslaunch startup.launch&
+
+elif [ "$1" == "mapping" ]
+then
+ echo "Mapping"
+ killall -9 mira
+
+ cd /opt/ros/hobbit_hydro/src/interfaces_mira/launch
+ roslaunch startup_ros_nav.launch&
+
+ cd /opt/ros/hobbit_hydro/src/interfaces_mira/resources
+ miracenter mira_config_ros_nav.xml&
+
+ cd /opt/ros/hobbit_hydro/src/navigation
+ roslaunch gmapping_hobbit.launch&
+
+elif [ "$1" == "navigation" ]
+then
+ echo "Regular Startup"
+ cd /opt/ros/hobbit_hydro/src/interfaces_mira/launch
+ roslaunch startup.launch&
+
+ cd /opt/ros/hobbit_hydro/src/interfaces_mira/resources
+ mira mira_config.xml -p1234&
+
+ cd /opt/ros/hobbit_hydro/src/interfaces_mira/resources
+ miracenter mira_vis_config.xml&
+
+fi
+
+
+exit 0
