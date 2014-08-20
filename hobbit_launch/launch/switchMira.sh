072bcbaa995a70210542bb4c9aca9c15e276141d
paloma.puente <paloma.puente@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-08-20 14:34:39 +0000

    changes to switchMira, needs testing
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7783 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_launch/launch/switchMira.sh
+++ hobbit_launch/launch/switchMira.sh
@@ -4,41 +4,57 @@ if [ "$#" -ne 1 ]; then
     echo "Illegal number of parameters, valid modes are learning,mapping,navigation"
 fi
 
-if [ "$1" == "learning" ]
-then
- echo "Learning"
-  killall -9 mira
-
-  cd /opt/ros/hobbit_hydro/src/interfaces_mira/launch
-  roslaunch startup.launch&
-
-  cd /opt/ros/hobbit_hydro/src/interfaces_mira/resources
-  miracenter mira_config.xml&
-
-  cd /opt/ros/hobbit_hydro/src/get_current_room/launch
-  roslaunch startup.launch&
-
-  cd /opt/ros/hobbit_hydro/src/places_learning_gui/launch
-  roslaunch startup.launch&
-
-elif [ "$1" == "mapping" ]
+if [ "$1" == "mapping" ]
 then
  echo "Mapping"
- killall -9 mira
- rosnode kill get_current_room&
+ killall -9 mira&
+ rosnode kill interfaces_node&
 
  cd /opt/ros/hobbit_hydro/src/interfaces_mira/launch
  roslaunch startup_ros_nav.launch&
 
  cd /opt/ros/hobbit_hydro/src/interfaces_mira/resources
- miracenter mira_config_ros_nav.xml&
+ mira mira_config_ros_nav.xml -p1234&
+
+ cd /opt/ros/hobbit_hydro/src/interfaces_mira/resources
+ miracenter mira_vis_config.xml&
 
  cd /opt/ros/hobbit_hydro/src/navigation
  roslaunch gmapping_hobbit.launch&
 
+elif [ "$1" == "learning" ]
+then
+ echo "Learning"
+ killall -9 mira&
+ rosnode kill interfaces_node&
+ rosnode kill gmapping_hobbit&
+ rosnode kill get_current_room&
+
+ cd /opt/ros/hobbit_hydro/src/interfaces_mira/launch
+ roslaunch startup.launch&
+
+ cd /opt/ros/hobbit_hydro/src/interfaces_mira/resources
+ mira mira_config.xml -p1234&
+
+ cd /opt/ros/hobbit_hydro/src/interfaces_mira/resources
+ miracenter mira_vis_config.xml&
+  
+ cd /opt/ros/hobbit_hydro/src/places_interpretation/launch
+ roslaunch startup.launch&
+
+ cd /opt/ros/hobbit_hydro/src/get_current_room/launch
+ roslaunch startup.launch&
+
+ cd /opt/ros/hobbit_hydro/src/places_learning_gui/launch
+ roslaunch startup.launch&
+
 elif [ "$1" == "navigation" ]
 then
  echo "Regular Startup"
+ # bajo: regular startup does not need the following nodes
+ # this is why they should be killed with rosnode kill
+ rosnode kill places_interpretation
+ rosnode kill get_current_rooms
  cd /opt/ros/hobbit_hydro/src/interfaces_mira/launch
  roslaunch startup.launch&
 
