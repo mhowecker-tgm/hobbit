1b73354127458f3af1f0f5b2eae038f70b936a87
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-08-20 08:34:52 +0000

    adding person aggregator
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7758 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- rgbd_acquisition/scripts/startFORTHStuffPT2.sh
+++ rgbd_acquisition/scripts/startFORTHStuffPT2.sh
@@ -62,7 +62,8 @@ screen -d -m -S "skeleton_detector" /bin/bash -c "source ~/.bashrc && source $HO
 echo "Starting up Fitness Function Coordinator"
 screen -d -m -S "fitness_coordinator" /bin/bash -c "source ~/.bashrc && source $HOBBITDIR/devel/setup.bash && roslaunch fitness_coordinator fitness_coordinator.launch"
  
-
+echo "Starting up Fitness Function Coordinator"
+screen -d -m -S "person_aggregator" /bin/bash -c "source ~/.bashrc && source /opt/ros/hobbit_hydro/devel/setup.bash && roslaunch person_aggregator person_aggregator.launch"
 
 
 
