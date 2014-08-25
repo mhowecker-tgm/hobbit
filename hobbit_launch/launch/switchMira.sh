fb400bdf2ffc1e764ee9e8cc2706f5827a9a6c84
paloma.puente <paloma.puente@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-08-25 15:47:47 +0000

    switchMira.sh modified, should launch things better
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7822 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_launch/launch/switchMira.sh
+++ hobbit_launch/launch/switchMira.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DELAY_BETWEEN_STEPS="5"
+
 if [ "$#" -ne 1 ]; then
     echo "Illegal number of parameters, valid modes are learning,mapping,navigation"
 fi
@@ -8,17 +10,24 @@ if [ "$1" == "mapping" ]
 then
  echo "Mapping"
  killall -9 mira&
+ sleep $DELAY_BETWEEN_STEPS
+ killall -9 miracenter&
+ sleep $DELAY_BETWEEN_STEPS
  rosnode kill interfaces_node&
-
+ 
+ sleep $DELAY_BETWEEN_STEPS
  cd /opt/ros/hobbit_hydro/src/interfaces_mira/launch
  roslaunch startup_ros_nav.launch&
 
+ sleep $DELAY_BETWEEN_STEPS
  cd /opt/ros/hobbit_hydro/src/interfaces_mira/resources
  mira mira_config_ros_nav.xml -p1234&
 
+ sleep $DELAY_BETWEEN_STEPS
  cd /opt/ros/hobbit_hydro/src/interfaces_mira/resources
  miracenter mira_vis_config.xml&
 
+ sleep $DELAY_BETWEEN_STEPS
  cd /opt/ros/hobbit_hydro/src/navigation
  roslaunch gmapping_hobbit.launch&
 
@@ -26,24 +35,34 @@ elif [ "$1" == "learning" ]
 then
  echo "Learning"
  killall -9 mira&
+ sleep $DELAY_BETWEEN_STEPS
+ killall -9 miracenter&
+ sleep $DELAY_BETWEEN_STEPS
  rosnode kill interfaces_node&
- rosnode kill gmapping_hobbit&
-
+ sleep $DELAY_BETWEEN_STEPS
+ rosnode kill slam_gmapping&
+ 
+ sleep $DELAY_BETWEEN_STEPS
  cd /opt/ros/hobbit_hydro/src/interfaces_mira/launch
  roslaunch startup.launch&
 
+ sleep $DELAY_BETWEEN_STEPS
  cd /opt/ros/hobbit_hydro/src/interfaces_mira/resources
  mira mira_config.xml -p1234&
 
+ sleep $DELAY_BETWEEN_STEPS
  cd /opt/ros/hobbit_hydro/src/interfaces_mira/resources
  miracenter mira_vis_config.xml&
   
+ sleep $DELAY_BETWEEN_STEPS
  cd /opt/ros/hobbit_hydro/src/places_interpretation/launch
  roslaunch startup.launch&
 
+ sleep $DELAY_BETWEEN_STEPS
  cd /opt/ros/hobbit_hydro/src/get_current_room/launch
  roslaunch startup.launch&
 
+ sleep $DELAY_BETWEEN_STEPS
  cd /opt/ros/hobbit_hydro/src/places_learning_gui/launch
  roslaunch startup.launch&
 
@@ -54,16 +73,23 @@ then
  # this is why they should be killed with rosnode kill
  rosnode kill places_interpretation&
  rosnode kill get_current_room&
+ sleep $DELAY_BETWEEN_STEPS
 
  killall -9 mira&
+ sleep $DELAY_BETWEEN_STEPS
+ killall -9 miracenter&
+ sleep $DELAY_BETWEEN_STEPS
  rosnode kill interfaces_node&
 
+ sleep $DELAY_BETWEEN_STEPS
  cd /opt/ros/hobbit_hydro/src/interfaces_mira/launch
  roslaunch startup.launch&
 
+ sleep $DELAY_BETWEEN_STEPS
  cd /opt/ros/hobbit_hydro/src/interfaces_mira/resources
- mira mira_config.xml -p1234&
+ mira mira_config2.xml -p1234&
 
+ sleep $DELAY_BETWEEN_STEPS
  cd /opt/ros/hobbit_hydro/src/interfaces_mira/resources
  miracenter mira_vis_config.xml&
 
