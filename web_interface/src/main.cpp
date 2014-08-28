47562c1e98a979b10bc6680f1c946bb0a789e7ec
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-08-28 10:57:43 +0000

    ...
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7860 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- web_interface/src/main.cpp
+++ web_interface/src/main.cpp
@@ -252,8 +252,8 @@ void * prepare_stats_content_callback(struct AmmServer_DynamicRequest  * rqst)
   char chargingState[MAX_COMMAND_SIZE]={0};
   getBackCommandLine((char*) "timeout 1 rostopic echo /battery_state -n 1 | grep charging | cut -d ':' -f2", chargingState , MAX_COMMAND_SIZE );
   unsigned int charging=atoi(chargingState);
-  if (charging)  { snprintf(batteryState,MAX_COMMAND_SIZE,"<img src=\"plugged.png\" height=\"15\"> %u ",charging); } else
-                 { snprintf(batteryState,MAX_COMMAND_SIZE," %u ",charging); }
+  if (charging)  { snprintf(chargingState,MAX_COMMAND_SIZE,"<img src=\"plugged.png\" height=\"15\"> %u ",charging); } else
+                 { snprintf(chargingState,MAX_COMMAND_SIZE," %u ",charging); }
 
 
   //  char mileageState[MAX_COMMAND_SIZE]={0};
