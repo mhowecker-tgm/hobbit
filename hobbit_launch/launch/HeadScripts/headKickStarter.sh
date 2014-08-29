914444c529ec00eac23b3c7a3324de26cd0c9b69
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-08-29 10:28:32 +0000

    ...
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7869 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_launch/launch/HeadScripts/headKickStarter.sh
+++ hobbit_launch/launch/HeadScripts/headKickStarter.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 source /home/pi/.bashrc
+
+./headSyncTime.sh
+
 while true; do
  nc6 -l -p 8080 -e "./headStart.sh"
  sleep 1
--- hobbit_launch/launch/HeadScripts/updateHead.sh
+++ hobbit_launch/launch/HeadScripts/updateHead.sh
@@ -4,10 +4,18 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
 cd /opt/ros/hobbit_hydro/src/hobbit_launch/launch/HeadScripts/
-scp headStart.sh pi@192.168.2.199:/home/pi/headStart.sh 
-scp headKickStarter.sh pi@192.168.2.199:/home/pi/headKickStarter.sh 
-scp activateHeadAutoStart.sh pi@192.168.2.199:/home/pi/activateHeadAutoStart.sh
-scp owlpose.py pi@192.168.2.199:/home/pi/workspace/blue_owlpose/scripts/owlpose.py
-scp herkulex.py pi@192.168.2.199:/home/pi/workspace/blue_owlpose/scripts/herkulex.py
+scp headStart.sh headKickStarter.sh systemCommandsPi.c activateHeadAutoStart.sh headSyncTime.sh pi@192.168.2.199:/home/pi
+scp owlpose.py herkulex.py pi@192.168.2.199:/home/pi/workspace/blue_owlpose/scripts
+
+
+#cd /opt/ros/hobbit_hydro/src/hobbit_launch/launch/HeadScripts/
+#scp headStart.sh pi@192.168.2.199:/home/pi/headStart.sh 
+#scp headKickStarter.sh pi@192.168.2.199:/home/pi/headKickStarter.sh 
+#scp systemCommandsPi.c pi@192.168.2.199:/home/pi/systemCommandsPi.c
+#scp systemCommandsPi.c pi@192.168.2.199:/home/pi/systemCommandsPi.c
+#scp activateHeadAutoStart.sh pi@192.168.2.199:/home/pi/activateHeadAutoStart.sh
+#scp owlpose.py pi@192.168.2.199:/home/pi/workspace/blue_owlpose/scripts/owlpose.py
+#scp herkulex.py pi@192.168.2.199:/home/pi/workspace/blue_owlpose/scripts/herkulex.py
+
 
 exit 0
