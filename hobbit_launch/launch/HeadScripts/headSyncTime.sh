20411da5aa9afa894c87c11b36b2f1d266adb263
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-08-29 14:48:16 +0000

    ...
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7879 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_launch/launch/HeadScripts/headSyncTime.sh
+++ hobbit_launch/launch/HeadScripts/headSyncTime.sh
@@ -2,8 +2,8 @@
 
 #we need to perform some super-user commands from a non root user , so we use oure systemCommandsPi suid binary to do those things 
 
-./systemCommandsPi chronyStart
-./systemCommandsPi ntpdate
 ./systemCommandsPi chronyStop
+./systemCommandsPi ntpdate
+./systemCommandsPi chronyStart
 
 exit 0
