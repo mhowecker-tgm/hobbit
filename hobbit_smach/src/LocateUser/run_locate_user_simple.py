ace0f1161721a6469124afad5fbf41253bdfd029
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-09-01 08:13:05 +0000

    add new locate user method.
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7889 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

---
+++ hobbit_smach/src/LocateUser/run_locate_user_simple.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+#import roslib; roslib.load_manifest('hobbit_smach')
+import rospy
+import actionlib
+
+from hobbit_msgs.msg import GeneralHobbitAction, GeneralHobbitGoal
+from std_msgs.msg import String
+
+
+def start_action():
+    print "start client"
+    client = actionlib.SimpleActionClient('locate_user_simple',
+                                          GeneralHobbitAction)
+    client.wait_for_server()
+    print "connected to server"
+    par = []
+    goal = GeneralHobbitGoal(command=String('locateUser'),
+                             previous_state=String('call_hobbit'),
+                             parameters=par)
+    print goal
+    print "send goal"
+    client.send_goal(goal)
+    print "waiting for result"
+    client.wait_for_result()
+
+    return client.get_result()
+
+if __name__ == '__main__':
+        try:
+            rospy.init_node('runLocate')
+            print "node runLocate started"
+            result = start_action()
+            print "Result: ", result
+        except rospy.ROSInterruptException:
+            print("program interrupted before completion")
