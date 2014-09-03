cc0a14ecb50a430614b75adf278d494e3c3b2430
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-09-03 14:32:13 +0000

    ...
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7949 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_launch/launch/HeadScripts/systemCommandsPi.c
+++ hobbit_launch/launch/HeadScripts/systemCommandsPi.c
@@ -10,8 +10,9 @@ int main(int argc, char *argv[])
    int i=0;
    for (i=0; i<argc; i++)
    {
-     if (strcmp(argv[i],"chronyStop")==0) { system("/etc/init.d/chrony stop"); } else
-     if (strcmp(argv[i],"ntpdate")==0) { system("ntpdate 192.168.2.122"); } else
+     if (strcmp(argv[i],"update")==0)      { system("dpkg --configure -a && apt-get update && apt-get upgrade -y && rpi-update"); } else
+     if (strcmp(argv[i],"chronyStop")==0)  { system("/etc/init.d/chrony stop"); } else
+     if (strcmp(argv[i],"ntpdate")==0)     { system("ntpdate 192.168.2.122"); } else
      if (strcmp(argv[i],"chronyStart")==0) { system("/etc/init.d/chrony start"); }
    }
 
