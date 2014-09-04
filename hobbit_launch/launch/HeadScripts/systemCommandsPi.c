4f687b5d22ce5bb594734914641871189eb56642
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-09-04 11:57:46 +0000

    raspberry pi systemCommands utility can now restart/shut it down
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7975 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_launch/launch/HeadScripts/systemCommandsPi.c
+++ hobbit_launch/launch/HeadScripts/systemCommandsPi.c
@@ -10,11 +10,14 @@ int main(int argc, char *argv[])
    int i=0;
    for (i=0; i<argc; i++)
    {
-     if (strcmp(argv[i],"update")==0)      { system("dpkg --configure -a && apt-get update && apt-get upgrade -y && rpi-update"); } else
-     if (strcmp(argv[i],"chronyStop")==0)  { system("/etc/init.d/chrony stop"); } else
-     if (strcmp(argv[i],"ntpdate")==0)     { system("ntpdate 192.168.2.122"); } else
-     if (strcmp(argv[i],"chronyStart")==0) { system("/etc/init.d/chrony start"); }
+     if (strcmp(argv[i],"update")==0)             { return system("dpkg --configure -a && apt-get update && apt-get upgrade -y && rpi-update"); } else
+     if (strcmp(argv[i],"restart")==0)            { return system("init 6"); } else
+     if (strcmp(argv[i],"shutdown")==0)           { return system("init 0"); } else
+     if (strcmp(argv[i],"shutdownOtherPi")==0)    { return system("echo notyet"); } else
+     if (strcmp(argv[i],"chronyStop")==0)         { return system("/etc/init.d/chrony stop"); } else
+     if (strcmp(argv[i],"ntpdate")==0)            { return system("ntpdate 192.168.2.122"); } else
+     if (strcmp(argv[i],"chronyStart")==0)        { return system("/etc/init.d/chrony start"); }
    }
 
-   return 0;
+   return 1;
 }
