39dde31c9cbc2e7d23ca150214d64834f357245e
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-09-04 11:31:59 +0000

    ...
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7973 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- web_interface/scripts/startWebInterface.sh
+++ web_interface/scripts/startWebInterface.sh
@@ -1,10 +1,41 @@
 #!/bin/bash
  
+HOBBITDIR="/opt/ros/hobbit_hydro/"
+SLEEP_TIME_IN_SECONDS_BETWEEN_RETRIES=2
+MAX_RETRIES=10
+
 #Script to start a screen session of web_interface
 
 echo "Starting up web_interface"
 screen -d -m -S "web_interface" /bin/bash -c "source ~/.bashrc && source /opt/ros/hobbit_hydro/devel/setup.bash && roslaunch web_interface web_interface.launch"
  
+sleep $SLEEP_TIME_IN_SECONDS_BETWEEN_RETRIES
+ 
+#We already tried once !
+i=2
+ISWEBINTFUP=`rosservice list | grep /web_interface/terminate`
+while [ -z "$ISWEBINTFUP" ]
+do  
+ echo "Web Interface node not started yet , try to wait for it $i/$MAX_RETRIES ( $ISWEBINTFUP )"
+ screen -d -m -S "web_interface" /bin/bash -c "source ~/.bashrc && source /opt/ros/hobbit_hydro/devel/setup.bash && roslaunch web_interface web_interface.launch"
+ sleep $SLEEP_TIME_IN_SECONDS_BETWEEN_RETRIES
+   
+ ISWEBINTFUP=`rosservice list | grep /web_interface/terminate`
+ 
+ ((i++))
+ if [ $i -gt $MAX_RETRIES ]
+ then 
+  echo "Maximum retries reached ,complete failure starting Web Interface service , todo try to restart a device or something here"
+  exit 1
+ fi
+done
+
+
+
+
+
+
+
 exit 0
 
 
