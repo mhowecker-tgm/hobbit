980e218fc86cc661acfd02abd9371ce90f5cc712
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-09-04 14:55:49 +0000

    ...
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@7997 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_launch/launch/HeadScripts/waitForRefresh.sh
+++ hobbit_launch/launch/HeadScripts/waitForRefresh.sh
@@ -3,7 +3,7 @@
 source /home/pi/.bashrc
 
 while true; do
- nc6 -l -p 8084 -e "echo \"Refreshing..\" && runSystemCommandPi.sh refresh && echo \"kthxbye..\""
+ nc6 -l -p 8084 -e "echo \"Refreshing..\" && ./runSystemCommandPi.sh refresh && echo \"kthxbye..\""
  sleep 1
 done
 
