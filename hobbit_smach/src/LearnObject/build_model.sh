573fc11a36b954301f466fd28c728bd1283bb926
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-09-15 13:11:54 +0000

    learn object cronjob added.
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@8120 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_smach/src/LearnObject/build_model.sh
+++ hobbit_smach/src/LearnObject/build_model.sh
@@ -3,12 +3,22 @@
 # author: Markus Bajones (bajo)
 # email: bajones@acin.tuwien.ac.at
 
-# This shell script will create the model and the recognizer structure 
+# This shell script will create the model and the recognizer structure
 # needed for the obejct recognition service
 
 # lower part of the object
-/opt/ros/acin/faat_framework/objectmodelbuild/hobbit_object_modeller /opt/ros/acin/faat_framework/object_modeller/hobbit_config.txt -reader.inputPath=/localhome/demo/data/${1}/down/ -writer.outputPath=/opt/ros/hobbit_hydro/src/recognition_service/recognition_structure/${1}_down.pcd/ -modelWriter.outputPath=/opt/ros/hobbit_hydro/src/recognition_service/models -modelWriter.pattern=${1}_down.pcd -reader.max_files=500
+/opt/ros/acin/faat_framework/objectmodelbuild/hobbit_object_modeller /opt/ros/acin/faat_framework/object_modeller/hobbit_config.txt \
+    -reader.inputPath=/localhome/demo/data/${1}/down/ \
+    -writer.outputPath=/opt/ros/hobbit_hydro/src/recognition_service/recognition_structure/${1}_down.pcd/ \
+    -modelWriter.outputPath=/opt/ros/hobbit_hydro/src/recognition_service/models \
+    -modelWriter.pattern=${1}_down.pcd \
+    -reader.max_files=500
 
 # upper part of the object
-/opt/ros/acin/faat_framework/objectmodelbuild/hobbit_object_modeller /opt/ros/acin/faat_framework/object_modeller/hobbit_config.txt -reader.inputPath=/localhome/demo/data/${1}/up/ -writer.outputPath=/opt/ros/hobbit_hydro/src/recognition_service/recognition_structure/${1}_up.pcd/ -modelWriter.outputPath=/opt/ros/hobbit_hydro/src/recognition_service/models -modelWriter.pattern=${1}_up.pcd -reader.max_files=500
+/opt/ros/acin/faat_framework/objectmodelbuild/hobbit_object_modeller /opt/ros/acin/faat_framework/object_modeller/hobbit_config.txt \
+    -reader.inputPath=/localhome/demo/data/${1}/up/ \
+    -writer.outputPath=/opt/ros/hobbit_hydro/src/recognition_service/recognition_structure/${1}_up.pcd/ \
+    -modelWriter.outputPath=/opt/ros/hobbit_hydro/src/recognition_service/models \
+    -modelWriter.pattern=${1}_up.pcd \
+    -reader.max_files=500
 
