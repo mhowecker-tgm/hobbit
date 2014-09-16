8d9af222926348e9dc7e722d03c816b9536e7171
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-09-16 15:14:20 +0000

    even less cpu time for advanced mode..
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@8146 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- skeleton_detector/src/main.cpp
+++ skeleton_detector/src/main.cpp
@@ -274,7 +274,7 @@ int main(int argc, char **argv)
      private_node_handle_.param("fromRGBTopicInfo", fromRGBTopicInfo, std::string("/headcam/rgb/camera_info"));
      private_node_handle_.param("name", name, std::string("skeleton_detector"));
      private_node_handle_.param("rate", rate, int(5));
-     ros::Rate loop_rate_ultra_low(2); //  hz should be our target performance
+     ros::Rate loop_rate_ultra_low(1); //  hz should be our target performance
      ros::Rate loop_rate(rate); //  hz should be our target performance
      unsigned int fastRate = rate*2;
      if (fastRate > 30) { fastRate=30; } //Cap fast speed at 30Hz ( i.e. frame rate of Depth camera )
