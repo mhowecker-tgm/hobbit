69e33631cea78427628248273a798ef9e949677c
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-09-17 16:12:58 +0000

    removed old rosrun commands
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@8171 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_launch/launch/startupHobbitPT2.sh
+++ hobbit_launch/launch/startupHobbitPT2.sh
@@ -72,23 +72,9 @@ roslaunch aal_service startup.launch&
 # rosparam load /opt/ros/hobbit_hydro/src/hobbit_params.yaml&
 # 
 # # Start SMACH handling of rooms, places, objects.
-# rosrun hobbit_smach places_objects.py&
-# roslaunch hobbit_smach SavePCD.launch&
-# rosrun hobbit_smach learn_object.py&
-# rosrun hobbit_smach away.py&
-# rosrun hobbit_smach goto.py&
-# rosrun hobbit_smach follow_me&
-# rosrun hobbit_smach emergency_user.py&
-# rosrun hobbit_smach emergency_bathroom.py&
-# rosrun hobbit_smach battery_monitor.py&
-# rosrun hobbit_smach recharge.py&
-# rosrun hobbit_smach sos_monitor.py&
-# rosrun hobbit_smach ArmActionServer.py&
-# 
 roslaunch hobbit_smach startup.launch&
 sleep $DELAY_BETWEEN_STEPS
 roslaunch hobbit_smach startup_master.launch&
-# rosrun hobbit_smach master.py&
 
 # Start table/floor object detector (for clustering) and trigger for publishing single shot point clouds from headcam
 cd /opt/ros/hobbit_hydro/src/table_object_detector/launch
