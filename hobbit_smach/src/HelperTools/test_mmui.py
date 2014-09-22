7016f42f4f442a44fd375cf778ac14dd0d6e4a40
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-09-22 17:21:05 +0000

    new test node to run through menues for surprise
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@8274 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

---
+++ hobbit_smach/src/HelperTools/test_mmui.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+PKG = 'hobbit_smach'
+NAME = 'test_mmui'
+
+import rospy
+
+from hobbit_msgs import MMUIInterface as MMUI
+
+
+def main():
+    rospy.init_node(NAME, anonymous=False)
+
+    menues = ['M_Audio, M_Entertain', 'M_Game',
+              'M_Memory', 'M_Solitaire', 'M_Chess',
+              'M_Simon', 'M_Sudoku', 'M_Muehle',
+              'M_eBook', 'M_Picture', 'M_Internet',
+              'M_Social', 'M_Checklist', 'M_Manual']
+
+    mmui = MMUI.MMUIInterface()
+    for menu in menues:
+        print(menu)
+        resp = mmui.GoToMenu(menu)
+        rospy.loginfo(str(resp))
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        return
