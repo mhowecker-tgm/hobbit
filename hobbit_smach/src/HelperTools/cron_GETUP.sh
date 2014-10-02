6a530ae557d95fbc2c985fb68386a94e7edc6cec
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-10-02 07:48:20 +0000

    add simple cronjob scripts
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@8356 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

---
+++ hobbit_smach/src/HelperTools/cron_GETUP.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source /localhome/demo/.bashrc
+
+rostopic pub -1 /Event hobbit_msgs/Event "header:
+  seq: 0
+  stamp: {secs: 0, nsecs: 0}
+  frame_id: ''
+event: 'E_GETUP'
+sessionID: ''
+confidence: 0.0
+params:
+- {name: '', value: ''}"
