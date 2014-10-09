a6922000aa1720526fabf630a55429d43f967a56
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-10-09 15:20:34 +0000

    Improve HOBBIT logging
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@8462 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_smach/src/hobbit_smach/logging_import.py
+++ hobbit_smach/src/hobbit_smach/logging_import.py
@@ -16,16 +16,21 @@ class DoLog(State):
     def __init__(self, scenario=None, data=None):
         State.__init__(
             self,
-            outcomes=['succeeded'],
-            input_keys=['scenario', 'data'] if
-            (scenario is None and data is None) else []
+            outcomes=['succeeded', 'aborted'],
+            input_keys=['scenario', 'data', 'command'] if
+            (scenario is None and data is None) else ['command']
         )
         self.scenario = scenario
         self.data = data
         self.pubEvent = rospy.Publisher('Event', Event, queue_size=50)
 
     def execute(self, ud):
-        scenario = ud.scenario if self.scenario is None else self.scenario
+        print ud.scenario
+        print self.scenario
+        print ud.command
+        if ud.scenario == 'IDLE' and ud.command == 'IDLE':
+            return 'aborted'
+        scenario = ud.scenario if self.scenario is not None else ud.command
         data = ud.data if self.data is None else self.data
         rospy.loginfo('LOG: scenario: %s: %s' % (scenario, data))
         message = Event()
@@ -90,4 +95,17 @@ class DoLogAborted(DoLog):
         )
         self.scenario = scenario
         self.data = 'Task has aborted.'
+        self.pubEvent = rospy.Publisher('Event', Event, queue_size=50)
+
+
+class DoLogStart(DoLog):
+    """
+    Inherit from DoLog but specialize in logging of Task started
+    """
+
+    def __init__(self):
+        DoLog.__init__(self)
+
+        self.scenario = None
+        self.data = 'Task has started.'
         self.pubEvent = rospy.Publisher('Event', Event, queue_size=50)
