46f4078c5cde80b74b852b31576510f57e3b84f7
david.fischinger <david.fischinger@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-12-03 16:40:29 +0000

    bug fixes
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@8838 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- calc_grasppoints_svm/src/calc_grasppoints_action_client.cpp
+++ calc_grasppoints_svm/src/calc_grasppoints_action_client.cpp
@@ -4,22 +4,24 @@
 #include <hobbit_msgs/CalcGraspPointsServerAction.h>
 
 
+//ROS includes
+#include <ros/package.h>
+#include "sensor_msgs/PointCloud2.h"
 
 
 
 class CCalcGrasppointsClient
 {
 public:
-	sensor_msgs::PointCloud2ConstPtr& point_cloud_global;
 	ros::Subscriber pc_sub;
 
 	void save_pc_cb(const sensor_msgs::PointCloud2ConstPtr& pc_in);
 
-	CCalcGrasppointsClient(nh)
+	CCalcGrasppointsClient(ros::NodeHandle nh_)
 	{
 
 		//subscriber for the pointcloud
-		this->pc_sub = nh_.subscribe("/SS/points2_object_in_rcs",1, &save_pc_cb, this);
+		this->pc_sub = nh_.subscribe("/SS/points2_object_in_rcs",1, &CCalcGrasppointsClient::save_pc_cb, this);
 	}
 };
 
@@ -39,7 +41,7 @@ void CCalcGrasppointsClient::save_pc_cb(const sensor_msgs::PointCloud2ConstPtr& 
 	ROS_INFO("Action server started, sending goal.");
 	// send a goal to the action
 	hobbit_msgs::CalcGraspPointsServerGoal goal;
-	goal.input_pc = pc_in;
+	goal.input_pc = *pc_in;
 	ac.sendGoal(goal);
 
 	//wait for the action to return
