e2ef91171dc74a71b6c6e6a0eab9b32ff83b8e7e
david.fischinger <david.fischinger@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2014-12-11 10:30:04 +0000

    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@8877 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_smach/src/PickUp/pickup_grasp_only.py
+++ hobbit_smach/src/PickUp/pickup_grasp_only.py
@@ -70,75 +70,6 @@ def point_cloud_cb(ud, msg):
 
 
 
-class CheckHelpAccepted(smach.State):
-    """
-    Just a counter in a SMACH State.
-    """
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['yes', 'no', 'preempted'])
-        self._help_accepted = 'no'
-
-    def execute(self, ud):
-        if self.preempt_requested():
-            rospy.set_param('/hobbit/help_accepted', 'no')
-            return 'preempted'
-        if rospy.has_param('/hobbit/help_accepted'):
-            self._help_accepted = rospy.get_param('/hobbit/help_accepted')
-        return self._help_accepted
-
-
-class PointingCounter(smach.State):
-    """
-    Just a counter in a SMACH State.
-    """
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['first', 'second'])
-        self._counter = 0
-
-    def execute(self, ud):
-        self._counter += 1
-        print('self._counter: %d' % self._counter)
-        if self._counter > 1:
-            self._counter = 0
-            return 'second'
-        else:
-            return 'first'
-
-
-class GraspCounter(smach.State):
-    """
-    Just a counter in a SMACH State.
-    """
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['first', 'second'])
-        self._counter = 0
-
-    def execute(self, ud):
-        self._counter += 1
-        print('self._counter: %d' % self._counter)
-        if self._counter > 1:
-            self._counter = 0
-            return 'second'
-        else:
-            return 'first'
-
-
-class MoveCounter(smach.State):
-    """
-    Just a counter in a SMACH State.
-    """
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['first', 'second'])
-        self._counter = 0
-
-    def execute(self, ud):
-        self._counter += 1
-        print('self._counter: %d' % self._counter)
-        if self._counter > 1:
-            self._counter = 0
-            return 'second'
-        else:
-            return 'first'
 
 
 class Init(smach.State):
@@ -158,72 +89,6 @@ class Init(smach.State):
         return 'succeeded'
 
 
-class CleanUp(smach.State):
-    """
-    Class for setting the result message and clean up persistent variables
-    """
-    def __init__(self):
-        smach.State.__init__(
-            self,
-            outcomes=['succeeded'],
-            input_keys=['command', 'visited_places'],
-            output_keys=['result', 'command', 'visited_places'])
-
-    def execute(self, ud):
-        ud.visited_places = []
-        ud.result = String('object not found')
-        return 'succeeded'
-
-
-class SetSuccess(smach.State):
-    """
-    Class for setting the success message in the
-    actionlib result and clean up of persistent variables
-    """
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['succeeded', 'preempted'],
-                             input_keys=['result'],
-                             output_keys=['result', 'visited_places'])
-
-    def execute(self, ud):
-        ud.visited_places = []
-        pub = rospy.Publisher('/DiscreteMotionCmd', String, queue_size=50)
-        rospy.sleep(1.0)
-        pub.publish('Stop')
-        if self.preempt_requested():
-            ud.result = String('preempted')
-            self.service_preempt()
-            return 'preempted'
-        ud.result = String('object detected')
-        rospy.loginfo(ud.result)
-        return 'succeeded'
-
-
-class DummyGrasp(smach.State):
-    def __init__(self):
-        smach.State.__init__(
-            self,
-            outcomes=['succeeded', 'failed', 'preempted'])
-
-    def execute(self, ud):
-        rospy.loginfo('Start DUMMY grasp')
-        if self.preempt_requested():
-            self.service_preempt()
-            return 'preempted'
-        return 'succeeded'
-
-
-def get_robot_pose_cb(msg, ud):
-    try:
-        pose = PoseStamped()
-        pose.header.frame_id = 'map'
-        pose.pose.position = msg.pose.pose.position
-        pose.pose.orientation = msg.pose.pose.orientation
-        ud.robot_current_pose = pose
-        return True
-    except:
-        print bcolors.FAIL + 'no robot pose message received.' + bcolors.ENDC
-        return False
 
 
 def main():
@@ -241,101 +106,22 @@ def main():
             'HEAD_TO_SEARCH',
             head_move.MoveTo(pose='to_grasp'),  # wait=True <=> df
             transitions={'succeeded': 'WAIT_FINISH_HEAD_TO_SEARCH', # df
-                         'aborted': 'EMO_SAY_OBJECT_NOT_DETECTED',
-                         'preempted': 'LOG_PREEMPT'}
+                         'aborted': 'LOG_ABORT',
+                         'preempted': 'LOG_ABORT'}
         )
         StateMachine.add(
             'WAIT_FINISH_HEAD_TO_SEARCH',
             SleepState(duration=10),
-            transitions={'succeeded': 'SAY_PICKING_UP',
-                         'preempted': 'LOG_PREEMPT'}
-            )
-        StateMachine.add(
-            'SAY_PICKING_UP',
-            speech_output.sayText(info='T_PU_PickingUpObject'),
             transitions={'succeeded': 'GRASP_OBJECT',
-                         'failed': 'EMO_SAY_DID_NOT_PICKUP',
-                         'preempted': 'LOG_PREEMPT'}
-        )
+                         'preempted': 'LOG_ABORT'}
+            )
         StateMachine.add(
             'GRASP_OBJECT',
             pickup.getPickupSeq(),
             # pickup.DavidPickingUp(),
-            transitions={'succeeded': 'CHECK_GRASP',
-                         'failed': 'EMO_SAY_DID_NOT_PICKUP',
-                         'preempted': 'LOG_PREEMPT'}
-        )
-        StateMachine.add(
-            'SAY_CHECK_GRASP',
-            speech_output.sayText(info='T_PU_CheckingGrasp'),
-            transitions={'succeeded': 'CHECK_GRASP',
-                         'failed': 'EMO_SAY_DID_NOT_PICKUP'}
-        )
-        StateMachine.add(
-            'CHECK_GRASP',
-            pickup.DavidCheckGrasp(),
-            transitions={'succeeded': 'END_PICKUP_SEQ',
-                         'aborted': 'COUNTER_GRASP_CHECK'}
-        )
-        StateMachine.add(
-            'COUNTER_GRASP_CHECK',
-            GraspCounter(),
-            transitions={'first': 'EMO_SAY_DID_NOT_PICKUP',
-                         'second': 'EMO_SAY_DID_NOT_PICKUP_2'}
-        )
-        StateMachine.add(
-            'EMO_SAY_DID_NOT_PICKUP',
-            pickup.sayDidNotPickupObject1(),
-            transitions={'succeeded': 'SAY_PICKING_UP',
-                         'failed': 'aborted'}
-        )
-        StateMachine.add(
-            'EMO_SAY_DID_NOT_PICKUP_2',
-            pickup.sayDidNotPickupObject2(),
-            transitions={'succeeded': 'aborted',
-                         'failed': 'aborted'}
-        )
-        StateMachine.add(
-            'END_PICKUP_SEQ',
-            pickup.getEndPickupSeq(),
-            transitions={'succeeded': 'CHECK_HELP',
-                         'failed': 'EMO_SAY_DID_NOT_PICKUP',
-                         'preempted': 'LOG_PREEMPT'}
-        )
-        StateMachine.add(
-            'CHECK_HELP',
-            CheckHelpAccepted(),
-            transitions={'yes': 'SAY_THANK_YOU',
-                         'no': 'SET_SUCCESS',
-                         'preempted': 'LOG_PREEMPT'}
-        )
-        StateMachine.add(
-            'SAY_THANK_YOU',
-            speech_output.sayText(info='T_PU_ThankYouPointing'),
-            transitions={'succeeded': 'SET_SUCCESS',
-                         'failed': 'SET_SUCCESS',
-                         'preempted': 'LOG_PREEMPT'}
-        )
-        StateMachine.add(
-            'SET_SUCCESS',
-            SetSuccess(),
-            transitions={'succeeded': 'LOG_SUCCESS',
-                         'preempted': 'CLEAN_UP'}
-        )
-        StateMachine.add(
-            'CLEAN_UP',
-            CleanUp(),
-            transitions={'succeeded': 'LOG_PREEMPT'}
-        )
-        StateMachine.add(
-            'LOG_SUCCESS',
-            log.DoLogSuccess(scenario='Pickup'),
-            transitions={'succeeded': 'succeeded'}
-        )
-        StateMachine.add(
-            'LOG_PREEMPT',
-            log.DoLogPreempt(scenario='Pickup'),
-            transitions={'succeeded': 'preempted'}
+            transitions={'aborted': 'CHECK_GRASP',
+                         'succeeded': 'LOG_ABORT',
+                         'preempted': 'LOG_ABORT'}
         )
         StateMachine.add(
             'LOG_ABORT',
