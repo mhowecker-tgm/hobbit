ed710cc2d4d9d70a912134d3770bd98108298ad2
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-01-09 16:41:38 +0000

    do switching between obstacle handling when head is up or down
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@9068 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_smach/src/hobbit_smach/hobbit_move_import.py
+++ hobbit_smach/src/hobbit_smach/hobbit_move_import.py
@@ -22,6 +22,7 @@ from move_base_msgs.msg import MoveBaseAction
 from hobbit_msgs.msg import MiraDockingAction, MiraDockingGoal
 from std_msgs.msg import String
 from mira_msgs.msg import BatteryState
+from mira_msgs.srv import UserNavMode, ObsNavMode
 from hobbit_user_interaction import HobbitMMUI
 from uashh_smach.util import SleepState, WaitForMsgState
 from actionlib import SimpleActionClient
@@ -467,6 +468,11 @@ def goToPosition(frame='/map', room='None', place='dock'):
             'HEAD_UP_AFTER_MOVEMENT',
             head_move.MoveTo(pose='center_center')
         )
+        Sequence.add('PREPARE_STOP',
+                     ServiceState('/user_nav_mode',
+                                  UserNavMode
+                     )
+        )
         # Sequence.add('ENABLE_GESTURES',
         #              service_disable.enable_gestures())
         Sequence.add(
@@ -502,6 +508,11 @@ def prepareMovement():
     with seq:
         # Sequence.add('DISABLE_GESTURES',
         #              service_disable.disable_gestures())
+        Sequence.add('PREPARE_MOVEMENT',
+                     ServiceState('/obs_nav_mode',
+                                  ObsNavMode
+                     )
+        )
         Sequence.add('HEAD_DOWN_BEFORE_MOVEMENT',
                      head_move.MoveTo(pose='down_center'))
         Sequence.add('WAIT', SleepState(duration=1))
@@ -555,6 +566,11 @@ def goToPose():
         Sequence.add(
             'HEAD_UP_AFTER_MOVEMENT',
             head_move.MoveTo(pose='center_center')
+        )
+        Sequence.add('PREPARE_STOP',
+                     ServiceState('/user_nav_mode',
+                                  UserNavMode
+                     )
         )
         # Sequence.add('ENABLE_GESTURES',
         #              service_disable.enable_gestures())
@@ -622,6 +638,11 @@ def goToPoseSilent():
         Sequence.add(
             'HEAD_UP_AFTER_MOVEMENT',
             head_move.MoveTo(pose='center_center')
+        )
+        Sequence.add('PREPARE_STOP',
+                     ServiceState('/user_nav_mode',
+                                  UserNavMode
+                     )
         )
         # Sequence.add('ENABLE_GESTURES',
         #              service_disable.enable_gestures())
