012f2752667b281ef5b590222b6fbf49bf61170b
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-01-21 12:01:17 +0000

    ...
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@9229 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- skeleton_detector/scripts/startSkeletonDetector.sh
+++ skeleton_detector/scripts/startSkeletonDetector.sh
@@ -1,46 +1,9 @@
 #!/bin/bash
  
-HOBBITDIR="/opt/ros/hobbit_hydro/"
-SLEEP_TIME_IN_SECONDS_BETWEEN_RETRIES=10
-MAX_RETRIES=10
-
-#Script to start a screen session of web_interface
+HOBBITDIR="/opt/ros/hobbit_hydro/" 
 
 echo "Starting up skeleton_detector"
 screen -d -m -S "skeleton_detector" /bin/bash -c "source ~/.bashrc && source /opt/ros/hobbit_hydro/devel/setup.bash && roslaunch skeleton_detector skeleton_detector.launch"
  
-
-exit 0
-
-
-sleep $SLEEP_TIME_IN_SECONDS_BETWEEN_RETRIES
- 
-#We already tried once !
-i=2
-ISWEBINTFUP=`rosservice list | grep /skeleton_detector/terminate`
-while [ -z "$ISWEBINTFUP" ]
-do  
- echo "Web Interface node not started yet , try to wait for it $i/$MAX_RETRIES ( $ISWEBINTFUP )"
- screen -d -m -S "skeleton_detector" /bin/bash -c "source ~/.bashrc && source /opt/ros/hobbit_hydro/devel/setup.bash && roslaunch skeleton_detector skeleton_detector.launch"
- sleep $SLEEP_TIME_IN_SECONDS_BETWEEN_RETRIES
-   
- ISWEBINTFUP=`rosservice list | grep /skeleton_detector/terminate`
- 
- ((i++))
- if [ $i -gt $MAX_RETRIES ]
- then 
-  echo "Maximum retries reached ,complete failure starting Web Interface service , todo try to restart a device or something here"
-  exit 1
- fi
-done
-
-
-
-
-
-
-
-exit 0
-
-
-
+  
+exit 0 
