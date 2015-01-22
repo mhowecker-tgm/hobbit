3dc54acd6504809a0e2c22fd9aa1b6e763bb3330
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-01-22 11:01:23 +0000

    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@9255 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

---
+++ make.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Easy way to compile as release without needing to remember the flags etc..
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+cd ..
+catkin_make -DCMAKE_BUILD_TYPE=Release
+
+
+exit 0
