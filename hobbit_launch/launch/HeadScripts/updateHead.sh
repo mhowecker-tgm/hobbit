69350b80ed3e47d428c00b2e98344b5b89ba9c8c
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-01-22 11:26:05 +0000

    ...
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@9260 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_launch/launch/HeadScripts/updateHead.sh
+++ hobbit_launch/launch/HeadScripts/updateHead.sh
@@ -3,8 +3,14 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
+echo "Trying to make head read write again..!"
+nc6 192.168.2.199 8092
+sleep 1
+echo "Done.."
+
+
 cd /opt/ros/hobbit_hydro/src/hobbit_launch/launch/HeadScripts/
-scp headStart.sh headKickStarter.sh runSystemCommandPi.sh waitForShutdown.sh waitForRefresh.sh systemCommandsPi.c checkStatus.sh activateHeadAutoStart.sh headSyncTime.sh pi@192.168.2.199:/home/pi
+scp headStart.sh headKickStarter.sh runSystemCommandPi.sh waitForShutdown.sh waitForRefresh.sh systemCommandsPi.c checkStatus.sh activateHeadAutoStart.sh headSyncTime.sh pi@192.168.2.199:/home_org/pi
 
 echo "Trying to force refresh"
 nc6 192.168.2.199 8084
