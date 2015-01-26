545c7f56dc3d29b9ffe3335112a57cac35312ddf
paloma.puente <paloma.puente@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-01-26 15:32:27 +0000

    client for come_closer testing
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@9318 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- client_test/src/client.cpp
+++ client_test/src/client.cpp
@@ -2,24 +2,27 @@
 #include <ros/ros.h>
 #include <actionlib/client/simple_action_client.h>
 #include <actionlib/client/terminal_state.h>
-#include <interfaces_mira/MiraDockingAction.h>
+//#include <interfaces_mira/MiraDockingAction.h>
+#include <hobbit_msgs/GeneralHobbitAction.h>
 
 int main (int argc, char **argv)
 {
   ros::init(argc, argv, "client_test");
 
   // create the action client
-  actionlib::SimpleActionClient<interfaces_mira::MiraDockingAction> ac("docking_task", true);
+  actionlib::SimpleActionClient<hobbit_msgs::GeneralHobbitAction> ac("come_closer", true);
 
   ROS_INFO("Waiting for action server to start.");
   // wait for the action server to start
   ac.waitForServer(); //will wait for infinite time
 
-  std::cout << "sending to recharge " << std::endl;
   ROS_INFO("Action server started, sending goal.");
   // send a goal to the action
-  interfaces_mira::MiraDockingGoal goal;
-  goal.docking_task = 0;
+  hobbit_msgs::GeneralHobbitGoal goal;
+  goal.command.data = "start";
+  goal.parameters.resize(2);
+  goal.parameters[0].data = "0.4";
+  goal.parameters[1].data = "0.4";
   ac.sendGoal(goal); 
 
   //wait for the action to return
