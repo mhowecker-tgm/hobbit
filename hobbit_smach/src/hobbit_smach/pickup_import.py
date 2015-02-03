32b3cf411d6571069e6920da3bee24318f3cf1db
david.fischinger <david.fischinger@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-02-03 14:05:33 +0000

    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@9429 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_smach/src/hobbit_smach/pickup_import.py
+++ hobbit_smach/src/hobbit_smach/pickup_import.py
@@ -947,7 +947,7 @@ class DavidCheckGrasp(State):
             input_keys=['cloud']
         )
         self.pubClust = rospy.Publisher("/pickup/checkgraspclusters", PointCloud2)
-        self.limit_x1 = 0.0
+        self.limit_x1 = -10.0
         self.limit_x2 = 1.15
         self.limit_y1 = -0.44
         self.limit_y2 = -1.55
