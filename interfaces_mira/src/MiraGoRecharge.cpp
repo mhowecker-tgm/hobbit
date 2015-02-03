0d4f3760b4ae380d9b7a5aeaf92e6b99731d6f76
david.fischinger <david.fischinger@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-02-03 10:58:59 +0000

    force changed
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@9410 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- interfaces_mira/src/MiraGoRecharge.cpp
+++ interfaces_mira/src/MiraGoRecharge.cpp
@@ -226,7 +226,7 @@ void MiraGoRecharge::executeCb(const interfaces_mira::MiraDockingGoalConstPtr& d
 
 				std::cout << "Docking on received" << std::endl;
 
-				mira::RPCFuture<void> r1 = robot_->getMiraAuthority().callService<void>("/robot/Robot#builtin", std::string("setProperty"), "MainControlUnit.Force", "20");
+				mira::RPCFuture<void> r1 = robot_->getMiraAuthority().callService<void>("/robot/Robot#builtin", std::string("setProperty"), "MainControlUnit.Force", "30");
         			r1.timedWait(mira::Duration::seconds(1));
         			r1.get();
 
