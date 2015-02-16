92965e8ca4955944c7772c34294847582d63816d
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-02-16 17:17:26 +0000

    master needed name change for watchdog. Follow up.
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@9614 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_smach/scripts/check_smach_nodes.sh
+++ hobbit_smach/scripts/check_smach_nodes.sh
@@ -7,15 +7,18 @@
 
 nodes=( "place_handler" "learn_object" "goto" "emergency_user"
 "emergency_bathroom" "battery_monitor" "recharge" "sos_monitor"
-"follow_me_simple" "pickup" "away" "master" "arm_action_server"
+"follow_me_simple" "pickup" "away_break" "puppetmaster" "arm_action_server"
 "bring_object" "fitness" )
 
 values=`rosnode list`
 for i in "${nodes[@]}"
 do
+    pid=`ps aux|grep ${i}|grep -v grep|awk {'print$2'}`
     # pid=`pidof -x ${i}`
+    echo ${pid}
     echo ${values}|grep ${i} 1> /dev/null
-    if [ $? -ne 0 ]; then
+    #if [ $? -ne 0 ]; then
+    if [ -z ${pid} ]; then
         echo -e "\e[31m${i} is not running\e[0m"
     else
         echo -e "\e[32m${i} is running\e[0m"
