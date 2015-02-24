69d62a677ed86e35d6c510a9c8bd7be4229f88de
david.fischinger <david.fischinger@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-02-24 15:20:02 +0000

    added node to calibrate head for navigation and grasping
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@9782 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

---
+++ table_object_detector/src/calibratehead.py
@@ -0,0 +1,196 @@
+#!/usr/bin/python
+
+## David Fischinger
+## first version: 24.2.2015
+# 
+# Node sets hobbit head to down_center position, 
+# takes point cloud (robot has to see pure plane in front)
+# messures average height on 2 positions infront of hobbit (1m and 2m away of hobbit)
+# adapts pitch offset (rosparameter)
+# sends command to owlpose to use new ros parameter for pitch offset
+# moves head away (needed because of week servos, small changes would not be executed) and back to (adapted) down_center position
+# this procedure is repeated until the height difference is below threshold or after n (5?) iterations
+# afterwards the scaling of the camera is adapted to best fit the ground plane in perception and real world
+
+
+
+# subscribes to /SS/doSingleShotTestCFS", if String comes in:
+#     subscribes to /headcam/depth_registered/points and uses point cloud for service call
+# publishes result (= number of points in defined area)
+# after that it unregisters subscriber of /headcam/depth_registered/points 
+
+
+
+PKG = 'hobbit_smach'
+import roslib; roslib.load_manifest(PKG)
+import rospy
+import subprocess
+import time, sys
+from std_msgs.msg import String
+from sensor_msgs.msg import PointCloud2
+from table_object_detector.srv import *
+
+
+class CalibrateHead():
+    def __init__(self, parent=None):
+    
+        self.heightdiff_th = 0.03   #threshold that is accepted for heigt difference on 2 different spots (in m)
+        self.pc_sub = None
+        #Publisher
+        self.headmove_pub = rospy.Publisher("/head/move", String )
+        self.actualize_head_offsets_pub = rospy.Publisher("/head/trigger/set_offsets", String)
+         
+        self.pc_ = None
+        t = None
+        #set space limits for cuboid 1 (near) robot 
+        self.limitc1_x1 =  0.9
+        self.limitc1_x2 =  1.1
+        self.limitc1_y1 = -0.1
+        self.limitc1_y2 =  0.1
+        self.limitc1_z1 = -0.3
+        self.limitc1_z2 =  0.3
+
+        #set space limits for cuboid 2 (away from) robot 
+        self.limitc2_x1 =  1.9
+        self.limitc2_x2 =  2.1
+        self.limitc2_y1 = -0.1
+        self.limitc2_y2 =  0.1
+        self.limitc2_z1 = -0.4
+        self.limitc2_z2 =  0.4
+        
+        self.nr_of_points_in_given_space = -1 # -1 <=> not correctly calculated, not valid
+        self.headmove_pub.publish(String("down_center"))
+        print "CalibrateHead.__init__() ===> Head is sent to down_center position, wait for some seconds"
+        rospy.sleep(5)  #wait for head movement
+    
+    #executes head movement commands
+    def move_head(self, cmd):
+        self.headmove_pub.publish(cmd)
+    
+    #triggers the process for getting single point cloud 
+    def start_calibration(self):
+        print "CalibrateHead.start calibration() ===> take single shot of environment"
+        self.nr_of_points_in_given_space = -1
+        self.t = None
+        #start subscriber
+        self.pc_sub = rospy.Subscriber("/headcam/depth_registered/points", PointCloud2, self.pc_callback, queue_size=1)
+   
+    
+    #starts method to publish point cloud for camera1; unregisters subscriber camera1
+    def pc_callback(self,msg):
+        self.pc_ = msg
+        self.pc_sub.unregister()
+        print "point cloud from camera saved"
+        #self.do_publish_cam1()
+  
+    #publishes pc for cam1
+    def get_average_z_value(self, cuboidnr=1):
+        print "get average z value of cuboid number ", cuboidnr
+        self.t = rospy.Time.now()
+        if self.pc_ == None:
+            print "CalibrateHead.get_average_z_value() ==> no point cloud found!"
+            return
+        self.pc_.header.stamp = self.t
+        #self.pc_pub.publish(self.pc_)
+        rospy.wait_for_service('check_mean_values_for_defined_space')
+    	try:
+            check_mean_values_for_defined_space = rospy.ServiceProxy('check_mean_values_for_defined_space', CheckMeanValuesForDefinedSpace)
+    	    input = CheckMeanValuesForDefinedSpace()
+    	    input.cloud = self.pc_
+    	    input.frame_id_original = String(self.pc_.header.frame_id)
+    	    #input.frame_id_desired = String(self.pc_.header.frame_id)
+    	    print "input.frame_id_original: ",input.frame_id_original
+    	    input.frame_id_desired = String("base_link")
+    	    print "input.frame_id_desired: ",input.frame_id_desired
+            if (cuboidnr == 1):
+        	    input.x1 = self.limitc1_x1
+        	    input.x2 = self.limitc1_x2
+        	    input.y1 = self.limitc1_y1
+        	    input.y2 = self.limitc1_y2
+        	    input.z1 = self.limitc1_z1
+        	    input.z2 = self.limitc1_z2
+            elif (cuboidnr == 2):
+                input.x1 = self.limitc2_x1
+                input.x2 = self.limitc2_x2
+                input.y1 = self.limitc2_y1
+                input.y2 = self.limitc2_y2
+                input.z1 = self.limitc2_z1
+                input.z2 = self.limitc2_z2
+            else:
+                print "CalibrateHead.get_average_z_value() ==> cuboidnr is neither 1 nor 2!"
+    	        return -1000
+            
+            resp1 = check_mean_values_for_defined_space(input.cloud,input.frame_id_original,input.frame_id_desired,input.x1,input.x2,input.y1,input.y2,input.z1,input.z2)
+    	    print "z-mean of points in area with boarders \nx1: ", input.x1, "\tx2: ",input.x2,"\ny1: ",input.y1,"\ty2: ",input.y2,"\nz1: ",input.z1,"\tz2: ",input.z2,"\nnr_points: ",resp1.z_mean
+    	except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+            
+        print "==================================\n result:", resp1
+        self.nr_of_points_in_given_space = resp1.nr_points_in_area
+        return resp1.z_mean
+
+
+    #get z-mean values for 2 positions and re-adjust head (change offset values)
+    def readjust_head(self, z_mean1, z_mean2):
+        offsetshift = 0
+        z_diff = z_mean1-z_mean2
+        if abs(z_diff) < self.heightdiff_th:   # plane is acceptably horizontal
+            return True
+        elif z_diff < -0.2:
+            offsetshift = 5
+        elif z_diff < -0.1:
+            offsetshift = 3
+        elif z_diff < -self.heightdiff_th:
+            offsetshift = 1            
+        elif z_diff > 0.2:
+            offsetshift = -5
+        elif z_diff > 0.1:
+            offsetshift = -3
+        elif z_diff > self.heightdiff_th:
+            offsetshift = -1
+         
+        cur_pitch_offset = rospy.get_param('/hobbit/head/pitch_offset', 0)
+        print "==> readjust_head: current pitch offset read from rosparameter: ", cur_pitch_offset   
+        print "==> readjust_head: set pitch offset to ", cur_pitch_offset+offsetshift
+        rospy.set_param('/hobbit/head/pitch_offset', cur_pitch_offset+offsetshift)
+        rospy.sleep(1)
+        self.actualize_head_offsets_pub.publish(String("asdf"))
+        rospy.sleep(3)
+        self.headmove_pub(String("center_center"))
+        rospy.sleep(3)
+        self.headmove_pub(String("down_center"))
+        rospy.sleep(3)
+        
+        return False
+        
+        
+
+def main(args):       
+    print "calibrate head (tf, depth scaling factor) node started" 
+    rospy.init_node('calibratehead_node', anonymous=False)
+
+    calibhead = CalibrateHead()
+    
+    for i in range(5):
+        print "calibration, iteration number: ", i
+        calibhead.start_calibration()
+        z_cuboid1 = calibhead.get_average_z_value(1)
+        print "main: ==> z_cuboid1", z_cuboid1
+        z_cuboid2 = calibhead.get_average_z_value(1)
+        print "main: ==> z_cuboid2", z_cuboid2
+        if calibhead.readjust_head(z_cuboid1, z_cuboid2):
+            print "adjusting head was successful"
+            break
+        elif i == 4:
+            print "adjusting head was NOT successful"
+    
+    #rospy.spin()
+    
+if __name__ == "__main__":        
+    main(sys.argv)
+
+
+
+   
+
+    
