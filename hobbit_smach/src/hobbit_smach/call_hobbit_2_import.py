89fd790c25285061ed72928aba01f5b9543c7b1b
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-03-04 18:42:26 +0000

    do not return message if it is not what we are waiting for
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@9959 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_smach/src/hobbit_smach/call_hobbit_2_import.py
+++ hobbit_smach/src/hobbit_smach/call_hobbit_2_import.py
@@ -183,10 +183,12 @@ def gesture_sm():
             return 'succeeded'
         elif outcome_map['TOPIC'] == 'succeeded':
             return 'succeeded'
+        elif outcome_map['TOPIC'] == 'succeeded':
+            return 'aborted'
         elif outcome_map['YES_NO'] in ['no', 'timeout', '3times', 'failed']:
             return 'aborted'
         else:
-            return 'aborted'
+            return 'preempted'
 
     cc1 = Concurrence(outcomes=['aborted', 'succeeded', 'preempted'],
                      default_outcome='aborted',
@@ -238,11 +240,11 @@ def gesture_sm():
         )
         Concurrence.add(
             'TOPIC',
-            WaitForMsgState(
+            WaitAndCheckMsgState(
                 '/Event',
                 Event,
                 msg_cb=event_cb,
-                timeout=10
+                timeout=28
             )
 
         )
@@ -250,19 +252,74 @@ def gesture_sm():
     return cc
 
 
-class CheckMsgState(WaitForMsgState):
+class WaitAndCheckMsgState(smach.State):
+    """This class acts as a generic message listener with blocking, timeout, latch and flexible usage.
+    It is meant to be extended with a case specific class that initializes this one appropriately
+    and contains the msg_cb (or overrides execute if really needed).
+    Its waitForMsg method implements the core functionality: waiting for the message, returning
+    the message itself or None on timeout.
+    Its execute method wraps the waitForMsg and returns succeeded or aborted, depending on the returned
+    message beeing existent or None. Additionally, in the successfull case, the msg_cb, if given, will
+    be called with the message and the userdata, so that a self defined method can convert message data to
+    smach userdata.
+    Those userdata fields have to be passed via 'output_keys'.
+    If the state outcome should depend on the message content, the msg_cb can dictate the outcome:
+    If msg_cb returns True, execute() will return "succeeded".
+    If msg_cb returns False, execute() will return "aborted".
+    If msg_cb has no return statement, execute() will act as described above.
+    If thats still not enough, execute() might be overridden.
+    latch: If True waitForMsg will return the last received message, so one message might be returned indefinite times.
+    timeout: Seconds to wait for a message, defaults to None, disabling timeout
+    output_keys: Userdata keys that the message callback needs to write to.
+    """
+
     def __init__(self, topic, msg_type, msg_cb=None, output_keys=None, latch=False, timeout=None):
         if output_keys is None:
             output_keys = []
-        smach.State.__init__(self, outcomes=['succeeded', 'aborted', 'preempted', 'timeout'], output_keys=output_keys)
+        smach.State.__init__(self, outcomes=['succeeded', 'aborted', 'preempted'], output_keys=output_keys)
         self.latch = latch
         self.timeout = timeout
         self.mutex = threading.Lock()
         self.msg = None
         self.msg_cb = msg_cb
         self.subscriber = rospy.Subscriber(topic, msg_type, self._callback, queue_size=1)
+
+    def _callback(self, msg):
+        self.mutex.acquire()
+        self.msg = msg
+        self.mutex.release()
+
+    def waitForMsg(self):
+        """Await and return the message or None on timeout."""
+        rospy.loginfo('Waiting for message...')
+        if self.timeout is not None:
+            timeout_time = rospy.Time.now() + rospy.Duration.from_sec(self.timeout)
+        while self.timeout is None or rospy.Time.now() < timeout_time:
+            self.mutex.acquire()
+            if self.msg is not None:
+                rospy.loginfo('Got message.')
+                message = self.msg
+
+                if not self.latch:
+                    self.msg = None
+
+                self.mutex.release()
+                if message == 'succeeded':
+                    return message
+            self.mutex.release()
+
+            if self.preempt_requested():
+                self.service_preempt()
+                rospy.loginfo('waitForMsg is preempted!')
+                return 'preempted'
+
+            rospy.sleep(.1) # TODO: maybe convert ROSInterruptException into valid outcome
+
+        rospy.loginfo('Timeout on waiting for message!')
+        return None
+
     def execute(self, ud):
-        """Tiny changes to default execute(), see class description."""
+        """Default simplest execute(), see class description."""
         msg = self.waitForMsg()
         if msg is not None:
             if msg == 'preempted':
@@ -278,8 +335,7 @@ class CheckMsgState(WaitForMsgState):
                         return 'aborted'
             return 'succeeded'
         else:
-            return 'timeout'
-
+            return 'aborted'
 
 def call_hobbit():
     """
