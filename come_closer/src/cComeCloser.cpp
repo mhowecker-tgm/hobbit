be8d3f09139987d38284967bd0c3273f03932f01
paloma.puente <paloma.puente@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-03-06 16:15:09 +0000

    not checking the navigation mode to check if movement finished, removed sleep
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@9968 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- come_closer/src/cComeCloser.cpp
+++ come_closer/src/cComeCloser.cpp
@@ -98,16 +98,16 @@ void cComeCloser::motion_state_callback(const std_msgs::String::ConstPtr& msg)
 	started_movement = true;
   if (started_movement && !finished_movement && current_motion_state.data == "Idle")
   {
-	bool dist_mode = true;
+	/*bool dist_mode = true;
 	mira_msgs::GetBoolValue get_nav_mode_srv;
   	if (get_nav_mode_client.call(get_nav_mode_srv))
 	{	
 		dist_mode = get_nav_mode_srv.response.value;
 	}
 	else
-		ROS_INFO("Failed to call service get_nav_mode");
+		ROS_INFO("Failed to call service get_nav_mode");*/
 		
-	if (!dist_mode)
+	//if (!dist_mode)
 	{
 		finished_movement = true;
 		std::cout << "finished movement " << std::endl;
@@ -297,7 +297,7 @@ void cComeCloser::executeCb(const hobbit_msgs::GeneralHobbitGoalConstPtr& goal)
 
 		if (finished_rotation && !started_movement && !movement_cmd_sent)
 		{
-			sleep(2);
+			//sleep(2);
 			std_msgs::String move_cmd;
 			std::ostringstream s;
 			s << "Move " << dis2move;
