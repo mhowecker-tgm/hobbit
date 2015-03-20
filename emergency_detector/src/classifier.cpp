dfead9a7aea1a0105f3edfe1d71758b3414dda95
forth <forth@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-03-20 10:14:37 +0000

    more strict over score..
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@10185 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- emergency_detector/src/classifier.cpp
+++ emergency_detector/src/classifier.cpp
@@ -22,7 +22,7 @@ unsigned int receivedBaseImages=0;
 
 int initializeClassifier()
 {
-  minimums.holesPercentOverTop=60;
+  minimums.holesPercentOverTop=70;
   maximums.holesPercentOverTop=100;
   lastState.useHolesOverTop=1;
 
