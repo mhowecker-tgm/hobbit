4c9e0cb81962b4eab159cce17fbb6f9f7eaae2ea
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-03-21 11:24:40 +0000

    do not say iamconing twice
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@10241 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_smach/src/hobbit_smach/fitness_import.py
+++ hobbit_smach/src/hobbit_smach/fitness_import.py
@@ -113,16 +113,8 @@ def get_do_fitness():
         StateMachine.add(
             'INIT',
             Init(),
-            transitions={'succeeded': 'SAY_MOVING_TO_YOU',
-                         'canceled': 'LOG_ABORTED'}
-        )
-        StateMachine.add(
-            'SAY_MOVING_TO_YOU',
-            #speech_output.sayText(info='T_FI_MovingToYou'),
-            speech_output.sayText(info='T_CA_IAmComing'),
             transitions={'succeeded': 'MOVE_BASE',
-                         'failed': 'LOG_ABORTED',
-                         'preempted': 'LOG_PREEMPT'}
+                         'canceled': 'LOG_ABORTED'}
         )
         StateMachine.add(
             'MOVE_BASE',
