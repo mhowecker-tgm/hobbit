127ed115b48ed9920970c5416c956cb030f87178
paloma.puente <paloma.puente@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-04-09 13:42:20 +0000

    added script to simplify places setup
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@10415 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

---
+++ navigation/sym_link.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DELAY_BETWEEN_STEPS="5"
+
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters, please provide the file name"
+fi
+
+rosnode kill place_handler
+cd /opt/ros/hobbit_hydro/src/navigation
+rm places.xml
+ln -s "$1" places.xml
+
+
+exit 0
