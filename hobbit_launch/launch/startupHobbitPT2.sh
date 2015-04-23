a2887202fa80dff6a64f9e330ac872a71d03c30c
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-04-23 09:35:00 +0000

    use one launch file instead of calling them one by one
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@10470 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_launch/launch/startupHobbitPT2.sh
+++ hobbit_launch/launch/startupHobbitPT2.sh
@@ -21,8 +21,7 @@ env >> ~/.startup.txt
 source ~/.bashrc
 source /opt/ros/hobbit_hydro/devel/setup.bash
 
-#roscore&
-roslaunch sqlitedb startup.launch&
+roscore&
 
 #ros core takes ages to start
 sleep $DELAY_BETWEEN_STEPS
@@ -37,15 +36,12 @@ rosparam set hobbit_id "$hobbit_id"
 rm /opt/ros/hobbit_hydro/src/configuration/active.yaml
 ln -s /opt/ros/hobbit_hydro/src/configuration/$hobbit_id.yaml  /opt/ros/hobbit_hydro/src/configuration/active.yaml
 rosparam load /opt/ros/hobbit_hydro/src/configuration/active.yaml
-sleep $DELAY_BETWEEN_STEPS
-roslaunch sqlitedb startup.launch&
+#roslaunch sqlitedb startup.launch&
 
 #Trying to start basecam
 /opt/ros/hobbit_hydro/src/rgbd_acquisition/scripts/startBaseCameraPT2.sh
 #If someone would like to start it on his own he could use the following
 #roslaunch openni2_launch openni2.launch camera:=basecam depth_registration:=true device_id:="#2"&
-sleep $DELAY_BETWEEN_STEPS
-
 
 
 #Bring up web interface after we got some things on , so that we can see what is happening ( port 8080 )
@@ -61,13 +57,11 @@ chmod +x /opt/ros/hobbit_hydro/src/web_interface/scripts/startWebInterface.sh
 /opt/ros/hobbit_hydro/src/rgbd_acquisition/scripts/startFORTHStuffPT2.sh
 #In case someone would like to startup the top camera without our integrated node he could do it like following
 #roslaunch openni2_launch openni2.launch camera:=headcam depth_registration:=true device_id:="#1"&
-sleep $DELAY_BETWEEN_STEPS
-
 
 
 # Start the virtual laser
-cd /opt/ros/hobbit_hydro/src/virtual_laser2/launch
-roslaunch startup.launch&
+#cd /opt/ros/hobbit_hydro/src/virtual_laser2/launch
+#roslaunch startup.launch&
 
 sleep $DELAY_BETWEEN_STEPS
 
@@ -75,22 +69,22 @@ sleep $DELAY_BETWEEN_STEPS
 ROBOTNUMBER=$( printf '%d' "'${hobbit_id:3:1}" ); let "ROBOTNUMBER -= 96"; echo "copy params$ROBOTNUMBER for PT2$ROBOTNAME"
 cd /opt/ros/hobbit_hydro/src/aal_service/launch
 cp params"$ROBOTNUMBER".yaml params.yaml
-roslaunch aal_service startup.launch&
+#roslaunch aal_service startup.launch&
 
 # Load Hobbit PT2 parameters
 # cd /opt/ros/hobbit_hydro/src/ #, web interface will save to this file also when somene clicks Set
 # rosparam load /opt/ros/hobbit_hydro/src/hobbit_params.yaml&
 # 
 # # Start SMACH handling of rooms, places, objects.
-roslaunch hobbit_smach startup.launch&
-sleep $DELAY_BETWEEN_STEPS
-roslaunch recognition_service startup.launch&
-sleep $DELAY_BETWEEN_STEPS
+#roslaunch hobbit_smach startup.launch&
+#sleep $DELAY_BETWEEN_STEPS
+#roslaunch recognition_service startup.launch&
+#sleep $DELAY_BETWEEN_STEPS
 # roslaunch hobbit_smach startup_master.launch&
 
 # Start table/floor object detector (for clustering) and trigger for publishing single shot point clouds from headcam
-cd /opt/ros/hobbit_hydro/src/table_object_detector/launch
-roslaunch startup.launch&
+#cd /opt/ros/hobbit_hydro/src/table_object_detector/launch
+#roslaunch startup.launch&
 
 #TODO : REMOVE ALL THESE ..
      # Start interfaces_mira, which starts the platform driver and the ros-mira interface for virtual lasers
@@ -108,24 +102,24 @@ roslaunch startup.launch&
 
 
 # Start the virtual laser from the headcam for obstacle avoidance
-cd /opt/ros/hobbit_hydro/src/top_scan_points/launch
-roslaunch startup.launch&
+#cd /opt/ros/hobbit_hydro/src/top_scan_points/launch
+#roslaunch startup.launch&
 
-sleep $DELAY_BETWEEN_STEPS
+#sleep $DELAY_BETWEEN_STEPS
 #Startup Joystick node , ( it allows webinterface joystick emulation also )
-roslaunch joy2twist startup.launch&
+#roslaunch joy2twist startup.launch&
 
 sleep $DELAY_BETWEEN_STEPS
 #Switch Mira stuff to navigation mode ( default )
 cd /opt/ros/hobbit_hydro/src/hobbit_launch/launch
 ./switchMira.sh navigation
 
-sleep $DELAY_BETWEEN_STEPS
-roslaunch localization_monitor startup.launch&
+#sleep $DELAY_BETWEEN_STEPS
+#roslaunch localization_monitor startup.launch&
 
-sleep $DELAY_BETWEEN_STEPS
+#sleep $DELAY_BETWEEN_STEPS
 # Start distance_to_obstacle calculation
-roslaunch distance_to_obstacle distance_to_obstacle.launch&
+#roslaunch distance_to_obstacle distance_to_obstacle.launch&
 
 sleep $DELAY_BETWEEN_STEPS
 #start action server node for calculating grasps (with HAF)
@@ -133,23 +127,27 @@ cd /opt/ros/hobbit_hydro/src/calc_grasppoints_svm
 rosrun calc_grasppoints_svm calc_grasppoints_action_server&
 
 sleep $DELAY_BETWEEN_STEPS
-cd /opt/ros/hobbit_hydro/src/top_scan_points/launch
-roslaunch startup.launch&
+#cd /opt/ros/hobbit_hydro/src/top_scan_points/launch
+#roslaunch startup.launch&
 
 #new 19.3.2015
 sleep $DELAY_BETWEEN_STEPS
 cd /opt/ros/hobbit_hydro/src/arm_simulation
 rosrun arm_simulation GraspFromFloorTrajectoryActionServer.py&
 
-sleep $DELAY_BETWEEN_STEPS
-roslaunch person_following startup.launch&
+#sleep $DELAY_BETWEEN_STEPS
+#roslaunch person_following startup.launch&
 
-sleep $DELAY_BETWEEN_STEPS
-roslaunch come_closer startup.launch&
+#sleep $DELAY_BETWEEN_STEPS
+#roslaunch come_closer startup.launch&
 
 rosrun dynamic_reconfigure dynparam set /basecam/driver data_skip 1&
 
+#sleep $DELAY_BETWEEN_STEPS
+#roslaunch sqlitedb startup.launch&
 
+sleep $DELAY_BETWEEN_STEPS
+roslaunch hobbit_launch hobbit.launch&
 
 #We have finally gone through this hole procedure , lets signal that hobbit is ready..
 rosservice call /web_interface/postMessage
