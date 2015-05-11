e3035d5423b8d58b8d213b4ffd822065e83952bd
michael.zillich <michael.zillich@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-05-11 08:02:11 +0000

    fixed bug with used counting variable not assigned (not commented as it was supposed to be)
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@10633 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- blue_owlpose/scripts/owlpose.py
+++ blue_owlpose/scripts/owlpose.py
@@ -376,7 +376,7 @@ def init():
             br.sendTransform( (0,0,0), tf.transformations.quaternion_from_euler(0, current_pitch/180.0*math.pi, current_yaw/180.0*math.pi), rospy.Time.now(), base_tf, head_tf)
             if debugPrintAngles:
                 print "owlpose pitch yaw [deg]: " + str(current_pitch) + "  " + str(current_yaw)
-        i = i + 1
+        #i = i + 1
         # NOTE: sometimes the tilt servo goes into torque limitation. Setting the torque limit again typically
         # wakes it up again. So we regularly set the limit, every second. So the neck should recover quickly
         # from a torque limit situation.
