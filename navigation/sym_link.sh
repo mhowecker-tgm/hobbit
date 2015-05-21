f7013ae12929d1f8374fcca5a03351fd5fbefeed
paloma.puente <paloma.puente@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-05-21 16:05:50 +0000

    sym link
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@10713 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- navigation/sym_link.sh
+++ navigation/sym_link.sh
@@ -6,9 +6,9 @@ if [ "$#" -ne 1 ]; then
     echo "Illegal number of parameters, please provide the file name"
 fi
 
-rosnode kill places_objects
+rosnode kill places_objects&
 sleep $DELAY_BETWEEN_STEPS
-rosnode kill places_object
+rosnode kill places_object&
 sleep $DELAY_BETWEEN_STEPS
 cd /opt/ros/hobbit_hydro/src/navigation
 rm places.xml
