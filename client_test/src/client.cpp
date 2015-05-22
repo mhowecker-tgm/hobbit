7061e87a828282e29eecf91b8f1e4a2b34f85995
paloma.puente <paloma.puente@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-05-22 14:46:07 +0000

    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@10719 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- client_test/src/client.cpp
+++ client_test/src/client.cpp
@@ -2,28 +2,28 @@
 #include <ros/ros.h>
 #include <actionlib/client/simple_action_client.h>
 #include <actionlib/client/terminal_state.h>
-//#include <interfaces_mira/MiraDockingAction.h>
-#include <hobbit_msgs/GeneralHobbitAction.h>
+#include <interfaces_mira/MiraDockingAction.h>
 
 int main (int argc, char **argv)
 {
   ros::init(argc, argv, "client_test");
 
   // create the action client
-  actionlib::SimpleActionClient<hobbit_msgs::GeneralHobbitAction> ac("come_closer", true);
+  actionlib::SimpleActionClient<interfaces_mira::MiraDockingAction> ac("docking_task", true);
 
   ROS_INFO("Waiting for action server to start.");
   // wait for the action server to start
   ac.waitForServer(); //will wait for infinite time
 
+  std::cout << "sending to recharge " << std::endl;
   ROS_INFO("Action server started, sending goal.");
   // send a goal to the action
-  hobbit_msgs::GeneralHobbitGoal goal;
-  goal.command.data = "start";
-  goal.parameters.resize(2);
-  goal.parameters[0].data = "0.4";
-  goal.parameters[1].data = "0";
+  interfaces_mira::MiraDockingGoal goal;
+  goal.docking_task = 0;
   ac.sendGoal(goal); 
+
+  /*sleep(30);
+  ac.cancelGoal();*/
 
   //wait for the action to return
   bool finished_before_timeout = ac.waitForResult(ros::Duration(60.0));
