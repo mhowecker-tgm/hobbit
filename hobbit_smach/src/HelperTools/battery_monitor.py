1c6df67f22d102256b08ab1b27d18048646ad908
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-05-26 09:16:27 +0000

    implement the head sleep caller
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@10739 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_smach/src/HelperTools/battery_monitor.py
+++ hobbit_smach/src/HelperTools/battery_monitor.py
@@ -4,12 +4,11 @@ import rospy
 from hobbit_msgs.msg import Event
 from std_msgs.msg import Header
 from mira_msgs.msg import BatteryState
-#from hobbit_msgs.srv import HeadSleep
+from hobbit_msgs.srv import HeadSleep
 
 NAME = 'battery_monitor'
 VOLT_LIMIT = 24.5
 LIMIT = False
-
 
 def talker(level):
     global LIMIT
@@ -26,19 +25,19 @@ def talker(level):
     pub.publish(data)
     rospy.loginfo('publish E_RECHARGE')
 
-#def call_service():
-#	rospy.wait_for_service('/head_sleep')
-#	head_sleep = rospy.ServiceProxy('/head_sleep', HeadSleep)
-#	try:
-#		  resp = head_sleep() 
-#	except rospy.ServiceException as exc:
-#		  print("Service did not process request: " + str(exc))
+def call_service():
+    rospy.wait_for_service('/head_sleep')
+    head_sleep = rospy.ServiceProxy('/head_sleep', HeadSleep)
+    try:
+        resp = head_sleep()
+    except rospy.ServiceException as exc:
+        print("Service did not process request: " + str(exc))
 
 def battery_cb(msg):
     global LIMIT
     if msg.charging:
         LIMIT = False
-	#call_service()
+        call_service()
     if msg.voltage < VOLT_LIMIT and msg.charging is False:
         rospy.loginfo('Battery level is low')
         talker('E_RECHARGE')
