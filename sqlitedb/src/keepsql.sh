aaadc80ab6f18093db5545150dc3a833282a2458
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-06-02 13:41:55 +0000

    source correct setup file
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@10776 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- sqlitedb/src/keepsql.sh
+++ sqlitedb/src/keepsql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /localhome/demo/.bashrc
+source /opt/ros/hobbit_hydro/devel/setup.bash
 
 cd /opt/ros/hobbit_hydro/src/sqlitedb/src/
 cp hobbit.db hobbit_${hobbit_id}_`date +%Y%m%d`.db
