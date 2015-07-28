af8e3afd8d68980e7dba4223ae9e8539a73be7ea
paloma.puente <paloma.puente@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-07-28 07:49:34 +0000

    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@11043 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_launch/launch/initialize_after_initial_setup.sh
+++ hobbit_launch/launch/initialize_after_initial_setup.sh
@@ -9,7 +9,7 @@ roslaunch localization_monitor startup.launch&
 sleep $DELAY_BETWEEN_STEPS
 rosrun hobbit_smach places_objects.py&
 sleep $DELAY_BETWEEN_STEPS
-rosnode kill distance_to_obstacle&
+rosnode kill /distance_to_obstacle&
 sleep $DELAY_BETWEEN_STEPS
 roslaunch distance_to_obstacle distance_to_obstacle.launch&
 sleep $DELAY_BETWEEN_STEPS
