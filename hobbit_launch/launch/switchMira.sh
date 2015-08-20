2a70b1bb159826a1d66bb1c4b7acd65747891c0c
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-08-20 09:07:02 +0000

    do not rely on rosnode kill. use killall -9 to do it correctly
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@11060 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

--- hobbit_launch/launch/switchMira.sh
+++ hobbit_launch/launch/switchMira.sh
@@ -19,11 +19,9 @@ LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/localhome/demo/mira/mira-hobbit/lib
 if [ "$1" == "mapping" ]
 then
  echo "Mapping"
+ killall -9 interfaces_node
  killall -9 mira&
- sleep $DELAY_BETWEEN_STEPS
  killall -9 miracenter&
- sleep $DELAY_BETWEEN_STEPS
- rosnode kill interfaces_node&
  
  sleep $DELAY_BETWEEN_STEPS
  cd /opt/ros/hobbit_hydro/src/interfaces_mira/launch
@@ -47,12 +45,9 @@ then
 elif [ "$1" == "learning" ]
 then
  echo "Learning"
+ killall -9 interfaces_node
  killall -9 mira&
- sleep $DELAY_BETWEEN_STEPS
  killall -9 miracenter&
- sleep $DELAY_BETWEEN_STEPS
- rosnode kill interfaces_node&
- sleep $DELAY_BETWEEN_STEPS
  rosnode kill slam_gmapping&
 sleep $DELAY_BETWEEN_STEPS
 rosnode kill places_objects&
@@ -92,23 +87,19 @@ then
  echo "Regular Startup"
  # bajo: regular startup does not need the following nodes
  # this is why they should be killed with rosnode kill
- rosnode kill places_interpretation&
- rosnode kill get_current_room&
- sleep $DELAY_BETWEEN_STEPS
-
+ #rosnode kill places_interpretation&
+ #rosnode kill get_current_room&
+ #sleep $DELAY_BETWEEN_STEPS
+ #rosnode kill interfaces_node&
+ killall -9 interfaces_node
  killall -9 mira&
- sleep $DELAY_BETWEEN_STEPS
+ #sleep $DELAY_BETWEEN_STEPS
  killall -9 miracenter&
- sleep $DELAY_BETWEEN_STEPS
- rosnode kill interfaces_node&
+ #sleep $DELAY_BETWEEN_STEPS
 
  sleep $DELAY_BETWEEN_STEPS
  cd /opt/ros/hobbit_hydro/src/interfaces_mira/resources
  mira mira_config2.xml -p1234&
-
- sleep $DELAY_BETWEEN_STEPS
- cd /opt/ros/hobbit_hydro/src/interfaces_mira/resources
- miracenter mira_vis_config.xml&
 
  sleep $DELAY_BETWEEN_STEPS
  sleep $DELAY_BETWEEN_STEPS
@@ -116,6 +107,14 @@ then
 
  cd /opt/ros/hobbit_hydro/src/interfaces_mira/launch
  roslaunch startup.launch&
+ 
+ sleep $DELAY_BETWEEN_STEPS
+ cd /opt/ros/hobbit_hydro/src/interfaces_mira/resources
+ miracenter mira_vis_config.xml&
+
+ sleep $DELAY_BETWEEN_STEPS
+ sleep $DELAY_BETWEEN_STEPS
+ rosservice call /obs_nav_mode "{}"&
 
 fi
 
