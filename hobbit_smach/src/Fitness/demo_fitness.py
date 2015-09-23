38d00edd4302a466a85efd30e73fcab4b12010dc
markus.bajones <markus.bajones@a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e>
2015-09-23 14:27:57 +0000

    add demo version of fitness and enable it in startup.launch
    git-svn-id: https://repo.acin.tuwien.ac.at/v4r/trunk/ros/hobbit_hydro_src@11062 a44b1c4d-ba22-49c9-afbd-3e401e1c4b3e

---
+++ hobbit_smach/src/Fitness/demo_fitness.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+PKG = 'hobbit_smach'
+NAME = 'demo_fitness'
+
+import rospy
+from smach import StateMachine
+from smach_ros import IntrospectionServer, ActionServerWrapper
+
+from hobbit_msgs.msg import GeneralHobbitAction
+import hobbit_smach.demo_fitness_import as fitness
+
+
+def main():
+    rospy.init_node(NAME)
+
+    fit_sm = StateMachine(
+        outcomes=['succeeded', 'aborted', 'preempted'],
+        input_keys=['command', 'parameters'],
+        output_keys=['result'])
+
+    with fit_sm:
+        StateMachine.add(
+            'START_FITNESS',
+            fitness.get_do_fitness(),
+            transitions={'succeeded': 'succeeded',
+                         'aborted': 'aborted',
+                         'preempted': 'preempted'}
+        )
+
+    asw = ActionServerWrapper(
+        'fitness', GeneralHobbitAction, fit_sm,
+        ['succeeded'], ['aborted'], ['preempted'],
+        result_slots_map={'result': 'result'},
+        goal_slots_map={'command': 'command',
+                        'parameters': 'parameters'})
+
+    #sis = IntrospectionServer(
+    #    'smach_server',
+    #    fit_sm,
+    #    '/HOBBIT/FIT_SM_ROOT')
+
+    #sis.start()
+    asw.run_server()
+    rospy.spin()
+    #sis.stop()
+
+if __name__ == '__main__':
+    main()
---
+++ hobbit_smach/src/hobbit_smach/demo_fitness_import.py
@@ -0,0 +1,201 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import rospy
+import uashh_smach.util as util
+
+from smach import StateMachine, State
+from hobbit_msgs.msg import Event
+from hobbit_msgs.srv import SwitchVision, SwitchVisionRequest
+from std_msgs.msg import String
+from smach_ros import ServiceState
+from hobbit_user_interaction import HobbitMMUI, HobbitEmotions
+import hobbit_smach.hobbit_move_import as hobbit_move
+import hobbit_smach.head_move_import as head_move
+import hobbit_smach.speech_output_import as speech_output
+import hobbit_smach.logging_import as log
+import hobbit_smach.call_hobbit_2_import as call_hobbit
+
+
+def switch_vision_cb(ud, response):
+    if response.result:
+        return 'succeeded'
+    else:
+        return 'aborted'
+
+
+class Init(State):
+    """
+    Class to initialize certain parameters
+    """
+
+    def __init__(self):
+        State.__init__(
+            self,
+            outcomes=['succeeded', 'canceled'],
+            input_keys=['command', 'parameters'],
+            output_keys=['social_role', 'object_name'])
+
+    def execute(self, ud):
+        if rospy.has_param('/hobbit/social_role'):
+            ud.social_role = rospy.get_param('/hobbit/social_role')
+        if ud.command.data == 'cancel':
+            return 'canceled'
+        return 'succeeded'
+
+
+class CleanUp(State):
+    """
+    Class for setting the result message and clean up persistent variables
+    """
+
+    def __init__(self):
+        State.__init__(
+            self,
+            outcomes=['succeeded'],
+            input_keys=['command', 'visited_places'],
+            output_keys=['result', 'command', 'visited_places'])
+        self.init = None
+
+
+    def execute(self, ud):
+        if not self.init:
+            self.init = True
+            self.pub_face = rospy.Publisher(
+                '/Hobbit/Emoticon',
+                String,
+                queue_size=50)
+            rospy.sleep( 1.0 )
+        self.pub_face.publish('EMO_SAD')
+        ud.visited_places = []
+        ud.result = String('object not detected')
+        return 'succeeded'
+
+
+class SetSuccess(State):
+    """
+    Class for setting the success message in the actionlib result and clean
+    up of persistent variables
+    """
+
+    def __init__(self):
+        State.__init__(
+            self,
+            outcomes=['succeeded', 'preempted'],
+            output_keys=['result', 'visited_places'])
+
+    def execute(self, ud):
+        ud.visited_places = []
+        if self.preempt_requested():
+            ud.result = String('preempted')
+            self.service_preempt()
+            return 'preempted'
+        return 'succeeded'
+
+
+def event_cb(msg, userdata):
+    rospy.loginfo('Got message with event: '+str(msg.event))
+    if msg.event.upper() in ['E_FITNESS_CLOSED', 'E_HELP', 'E_CANCEL']:
+        return True
+    else:
+        return False
+
+
+def get_do_fitness():
+    sm = StateMachine(
+        outcomes=['succeeded', 'aborted', 'preempted'],
+        input_keys=['command', 'parameters'],
+        output_keys=['result'])
+
+    sm.userdata.result = String('started')
+    sm.userdata.detection = False
+
+    with sm:
+        StateMachine.add(
+            'INIT',
+            Init(),
+            transitions={'succeeded': 'SWITCH_VISION_TO_FITNESS',
+                         'canceled': 'LOG_ABORTED'}
+        )
+        StateMachine.add_auto(
+            'SWITCH_VISION_TO_FITNESS',
+            ServiceState(
+                '/vision_system/fitnessFunction',
+                SwitchVision,
+                request=SwitchVisionRequest(dummyInput=True),
+                response_cb=switch_vision_cb
+            ),
+            connector_outcomes=['succeeded', 'preempted', 'aborted']
+        )
+        StateMachine.add(
+            'MOVE_HEAD_UP',
+            head_move.MoveTo(pose='littledown_center'),
+            transitions={'succeeded': 'DO_FITNESS',
+                         'preempted': 'LOG_PREEMPT',
+                         'aborted': 'DO_FITNESS'}
+        )
+        StateMachine.add(
+            'DO_FITNESS',
+            HobbitMMUI.ShowMenu(
+                menu='FITNESS'
+            ),
+            transitions={'succeeded': 'WAIT_FOR_END',
+                         'failed': 'LOG_ABORTED',
+                         'preempted': 'LOG_PREEMPT'}
+        )
+        StateMachine.add(
+            'WAIT_FOR_END',
+            util.WaitForMsgState(
+                '/Event',
+                Event,
+                msg_cb=event_cb),
+            transitions={'succeeded': 'SHOW_MAIN',
+                         'aborted': 'WAIT_FOR_END',
+                         'preempted': 'LOG_PREEMPT'}
+        )
+        StateMachine.add(
+            'SHOW_MAIN',
+            HobbitMMUI.ShowMenu(
+                menu='MAIN'
+            ),
+            transitions={'succeeded': 'BACK_TO_USER',
+                         'failed': 'LOG_ABORTED',
+                         'preempted': 'LOG_PREEMPT'}
+        )
+        StateMachine.add(
+            'BACK_TO_USER',
+            call_hobbit.come_closer_from_everywhere(),
+            transitions={'succeeded': 'LOG_SUCCESS',
+                         'aborted': 'WAIT_FOR_END',
+                         'preempted': 'LOG_PREEMPT'}
+        )
+        StateMachine.add(
+            'LOG_SUCCESS',
+            log.DoLogSuccess(scenario='Fitness'),
+            transitions={'succeeded': 'succeeded'}
+        )
+        StateMachine.add(
+            'LOG_PREEMPT',
+            log.DoLogPreempt(scenario='Fitness'),
+            transitions={'succeeded': 'preempted'}
+        )
+        StateMachine.add(
+            'LOG_ABORTED',
+            log.DoLogAborted(scenario='Fitness'),
+            transitions={'succeeded': 'aborted'}
+        )
+        StateMachine.add(
+            'SET_SUCCESS',
+            SetSuccess(),
+            transitions={'succeeded': 'succeeded',
+                         'preempted': 'CLEAN_UP'}
+        )
+        StateMachine.add(
+            'CLEAN_UP',
+            CleanUp(),
+            transitions={'succeeded': 'preempted'}
+        )
+    return sm
+
+if __name__ == '__main__':
+    print("Do not call this directly. Import it into your node.")
